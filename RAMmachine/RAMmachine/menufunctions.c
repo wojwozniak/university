1a5b21cf0387e9fa7d06d87b9bbe3dbf13334a5c
Wojtek <pulchnymamut@gmail.com>
2023-01-12 18:19:46 +0100

    typo

--- RAMmachine/RAMmachine/menufunctions.c
+++ RAMmachine/RAMmachine/menufunctions.c
@@ -2,7 +2,7 @@ void clearMemory(exe *tape, memory_cell *battery) {
     while(battery->next != NULL) {
         pop_last_cell(battery);
     }
-    while(main_tape->next != NULL) {
+    while(tape->next != NULL) {
         pop_last_exe(tape);
     }
     return;
