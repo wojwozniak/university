2bb5c07887b28ae56573d90dc8d28979df6f82a6
Wojtek <pulchnymamut@gmail.com>
2023-01-13 22:29:36 +0100

    added reading/writing files

--- RAMmachine/RAMmachine/menufunctions.c
+++ RAMmachine/RAMmachine/menufunctions.c
@@ -7,3 +7,39 @@ void clearMemory(exe *tape, memory_cell *battery) {
     }
     return;
 }
+
+int checkIfArgv(int argc) {
+    if(argc <= 2) {
+        return 0;
+    } else {
+        return argc-2;
+    }
+}
+
+bool readfile(char filename[100], exe *input_tape) {
+    FILE *textfile = fopen(filename, "r");
+    if(textfile == NULL) {
+        printf("ERROR! Nie mozna otworzyc pliku %s!\n", filename);
+        return 1;
+    }
+    char x[10];
+    while(fscanf(textfile, " %9s", x) == 1) {
+        attach_to_end(input_tape, "", x, "");
+    }
+    return 0;
+}
+
+bool write_output(char filename[100], exe *output_tape) {
+    FILE *textfile = fopen(filename, "w");
+    if(textfile == NULL) {
+        printf("ERROR! Nie mozna otworzyc pliku %s!\n", filename);
+        return 1;
+    } else {
+        while(output_tape!=NULL) {
+            fprintf(textfile, output_tape->payload);
+            output_tape = output_tape->next;
+        }
+        fclose(textfile);
+    }
+    return 0;
+}
--- RAMmachine/RAMmachine/menuheaders.h
+++ RAMmachine/RAMmachine/menuheaders.h
@@ -1,1 +1,4 @@
 void clearMemory(exe *tape, memory_cell *battery);
+int checkIfArgv(int argc);
+bool readfile(char filename[100], exe *input_tape);
+bool write_output(char filename[100], exe *output_tape);
