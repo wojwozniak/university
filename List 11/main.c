dbae0777a96dc0633326690b2506af910a8150ec
Wojtek <pulchnymamut@gmail.com>
2023-01-15 14:51:11 +0100

    1

--- List 11/main.c
+++ List 11/main.c
@@ -8,6 +8,25 @@ typedef struct node {
     pnode right;
 } snode;
 
+/* Task 1
+Glebokosc drzewa BST bedzie zalezec od kolejnosci w ktorej
+wstawiamy elementy.
+
+Najwieksza glebokosc uzyskamy zaczynajac
+od najmniejszego lub najwiekszego elementu - wowczas wszystkie
+elementy zawsze beda po jednej stronie - glebokosc wyniesie n
+
+Dla najmniejszej glebokosci
+a) 4->3->5->2->6->1->7
+b) Zaczynamy od 2^k/2, jeden w lewo, jeden w prawo
+Nie musi byc koniecznie taka kolejnosc po srodku - Ale
+pozwala ona stworzyc prosty algorytm wstawiania elementow
+Glebokosc drzewa: k
+
+*/
+
+
+
 int main() {
     return 0;
 }
