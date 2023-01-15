910b09e540f8d937efadbb353498cae7d1833f91
Wojtek <pulchnymamut@gmail.com>
2023-01-15 14:43:36 +0100

    Create main.c

---
+++ List 11/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node *pnode;
+typedef struct node {
+    int val;
+    pnode left;
+    pnode right;
+} snode;
+
+int main() {
+    return 0;
+}
