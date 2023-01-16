57c3ec980499d6ae3e06d237e7f75a8aa8e38fc6
Wojtek <pulchnymamut@gmail.com>
2023-01-16 23:12:40 +0100

    Update main.c

--- taskLists/List 10/l10z2/main.c
+++ taskLists/List 10/l10z2/main.c
@@ -8,43 +8,39 @@ int absolute(int a) {
     return a;
 }
 
-void printit(long x, int depth) {
+int printit(long x, int depth) {
+    int output = 0;
     if(depth < 5) {
-        printit(x/10, depth+1);
+        output += printit(x/10, depth+1);
     }
-    printf("%li", x%10);
+    output*=10;
+    output += x%10;
+    return output;
 }
 
 int main() {
     int a, deg, arg, abc;
     int vars[65536];
-    //printf("How many?\n");
     abc = scanf("%d", &a);
-    //printf("%d times\n", a);
     long num;
     long walking;
     for(int i=0; i<a; i++) {
         num = 0;
         walking = 1;
+        fflush(stdin);
         abc = scanf("%d %d", &deg, &arg);
-        //printf("Degree %d, arg %d\n", deg, arg);
         for(int b=0; b<=deg; b++) {
             abc = scanf("%d", &vars[b]);
         }
         for(int b=deg; b>=0; b--) {
-            //printf("Walking: %d \n", walking);
-            //printf("%d\n", vars[b]);
             long add = vars[b]*walking;
-            //printf("Add %d\n", add);
             num+=add;
-            //printf("Now %d\n", num);
             walking*=arg;
         }
         if(absolute(num) < 100000) {
             printf("%05d\n", absolute(num));
         } else {
-            printit(absolute(num), 1);
-            printf("\n");
+            printf("%05d\n", printit(absolute(num), 1));
         }
     }
     a = abc;
