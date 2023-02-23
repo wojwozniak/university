549e3fbddaa94ad4a4512efb2d661bce81f36fa7
Wojtek <pulchnymamut@gmail.com>
2023-02-23 22:16:18 +0100

    dividing into files, signing

---
+++ List01/Task1/headers.h
@@ -0,0 +1,11 @@
+/*
+Wojciech WoŸniak
+Lista 1: zadanie 1
+gnu gcc [-std=11]
+*/
+typedef struct Figura Figura;
+Figura *setup_fig();
+float dl_boku(float x1, float x2, float y1, float y2);
+Figura *new_circle(float r, float x, float y);
+Figura *new_triangle(float a, float b, float c, float d, float e, float f);
+Figura *new_square(float a, float b, float c, float d, float e, float f);
