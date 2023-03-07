a45a8a3ae290d68211645d4742f6c5ee4fc21153
Wojtek <pulchnymamut@gmail.com>
2023-03-07 10:32:20 +0100

    Update task2.rkt

--- List02/task2.rkt
+++ List02/task2.rkt
@@ -5,7 +5,8 @@
 (define (fib n)
     (cond [(= n 0) 0]
     [(= n 1) 1]
-    [else (+ (fib (- n 1)) (fib (- n 2)))])
+    [else (+ (fib (- n 1)) (fib (- n 2)))]
+  )
 )
 
 ; Testy wersji rekurencyjnej
