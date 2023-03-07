32e5d24fc02003b502d994a03734337191d1cc3b
Wojtek <pulchnymamut@gmail.com>
2023-03-07 11:23:18 +0100

    Update task6.rkt

--- List02/task6.rkt
+++ List02/task6.rkt
@@ -3,17 +3,14 @@
 ; Funkcja maximum - przyjmuje listę jako argument
 (define (maximum xs)
     ; Jeśli lista jest pusta zwraca -inf.0 - w przeciwnym przypadku zaczyna wywoływać funkcję rekurencyjną (parametry to lista oraz maksimum zainicjalizowane do -inf.0)
-    (if (null? xs)
-        -inf.0
-        (let rec ((xs xs) (max -inf.0))
-            ; Jeśli nie ma więcej elementów na liście zwracamy zero - w przeciwnym przypadku porównujemy aktualne maksimum z pierwszym elementem pary, a następnie wywołujemy rekurencję z odpowiednimi argumentami
-            (if (null? xs)
-                max
-                (let ((x (car xs)))
-                    (if (> x max)
-                        (rec (cdr xs) x)
-                        (rec (cdr xs) max)
-                    )
+    (let rec ((xs xs) (max -inf.0))
+        ; Jeśli nie ma więcej elementów na liście zwracamy zero - w przeciwnym przypadku porównujemy aktualne maksimum z pierwszym elementem pary, a następnie wywołujemy rekurencję z odpowiednimi argumentami
+        (if (null? xs)
+            max
+            (let ((x (car xs)))
+                (if (> x max)
+                    (rec (cdr xs) x)
+                    (rec (cdr xs) max)
                 )
             )
         )
