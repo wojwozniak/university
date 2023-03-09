1950242a80dada48117bf4241994bc34cf9e2ea8
Wojtek <pulchnymamut@gmail.com>
2023-03-09 11:54:35 +0100

    Update task7.rkt

--- List03/task7.rkt
+++ List03/task7.rkt
@@ -7,5 +7,6 @@
         xs 
     ) 
 )
-; Tworzy n-1 consów o łącznej długości 49995000 (długości 1,2,3...,9999 => ((a1+an)/2)*n)
+; Tworzy n-1 consów o łącznej długości (n^2/2)
+; W podanym przykładzie 9999 consów o długości ~5000000 (długości list 1,2,3...,9999 => ((a1+an)/2)*n)
 
