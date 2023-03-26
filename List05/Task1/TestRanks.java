dda4878402a0aaf9017e9fc3b5749468b21c93a6
Wojtek <pulchnymamut@gmail.com>
2023-03-26 16:49:17 +0200

    Update TestRanks.java

--- List05/Task1/TestRanks.java
+++ List05/Task1/TestRanks.java
@@ -1,18 +1,17 @@
 package List05.Task1;
 
-
 public class TestRanks {
     public static void main(String[] args) {
         MilitaryRank rank1 = new Szeregowy();
         MilitaryRank rank2 = new General();
 
         // Test for getName() method
-        System.out.println(rank1.getName().equals("Szeregowy")); // expected output: true
-        System.out.println(rank2.getName().equals("General")); // expected output: true
+        System.out.println(rank1.getName().equals("Szeregowy")); 
+        System.out.println(rank2.getName().equals("General")); 
 
         // Test for compareTo() method
-        System.out.println(rank1.compareTo(rank2) < 0); // expected output: true
-        System.out.println(rank2.compareTo(rank1) > 0); // expected output: true
-        System.out.println(rank1.compareTo(rank1) == 0); // expected output: true
+        System.out.println(rank1.compareTo(rank2) < 0);
+        System.out.println(rank2.compareTo(rank1) > 0); 
+        System.out.println(rank1.compareTo(rank1) == 0); 
     }
 }
