9a7bd7ebcc147879db43aaf820d66f7ede77366d
Wojtek <pulchnymamut@gmail.com>
2023-03-26 17:05:26 +0200

    update

--- List05/Task1/MilitaryRank.java
+++
@@ -1,8 +0,0 @@
-package List05.Task1;
-
-// Obchodzimy błąd najpierw korzystając
-// z extends, a później klasy abstrakcyjnej
-// i 'implements'
-public interface MilitaryRank extends Comparable<MilitaryRank> {
-    String getName();
-}
