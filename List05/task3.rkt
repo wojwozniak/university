ab15d5c6e240b4f24a8dc3f10b8a0eb351700423
Wojtek <pulchnymamut@gmail.com>
2023-03-27 13:11:45 +0200

    Update task3.rkt

--- List05/task3.rkt
+++ List05/task3.rkt
@@ -2,8 +2,17 @@
 
 ; Napisz i uzasadnij typy poniższych, 
 ; wykorzystujących procedury z poprzedniego
-; zadania, wyrażeń.
+; zadania, wyrażeń:
+;
+; ( curry compose )
+; (( curry compose ) ( curry compose ) )
+; (( curry compose ) ( curry apply ) )
+; (( curry apply ) ( curry compose ) )
+; ( compose curry flip )
 
+
+; Typy z poprzedniego zadania:
+;
 ; apply: (('a -> 'b) -> 'a -> 'b)
 ; compose: (('a -> 'b) ('c -> 'a) -> ('c -> 'b))
 ; flip: ((('a 'b) -> 'c) -> (('b 'a) -> 'c))
@@ -25,3 +34,23 @@
 ; (('x -> 'y) -> (('z -> 'x) -> ('z -> 'y)))
 ; co po postawieniu z powrotem daje nam
 ; ('a -> ('b -> 'c))
+
+
+; b) ((curry compose) (curry compose))
+; Wiemy już, że curry compose ma typ:
+; (('_a -> '_b) -> (('_c -> '_a) -> ('_c -> '_b)))
+; Aby uniknąć pomyłki, pod drugie curry compose 
+; podstawiamy inne nazwy zmiennych (jak w a)), 
+; otrzymując:
+; (('_x -> '_y) -> (('_z -> '_x) -> ('_z -> '_y)))
+;
+; Argument curry compose ma typ ('_a -> '_b),
+; do podstawienia mamy: 
+; (('_x -> '_y) -> (('_z -> '_x) -> ('_z -> '_y))),
+; otrzymujemy więc:
+; '_a = ('_x -> '_y)
+; '_b = (('_z -> '_x) -> ('_z -> '_y))
+; patrzymy na typ zwracany (('_c -> '_a) -> ('_c -> '_b)), 
+; podstawiamy zmienne:
+; (('_c -> ('_x -> '_y)) -> ('_c -> (('_z -> '_x) -> ('_z -> '_y))))
+; wszystko się zgadza
