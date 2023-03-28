f8ed723505196a5c7ff9074e39b00330ce4d1fa5
Wojtek <pulchnymamut@gmail.com>
2023-03-28 18:48:13 +0200

    default -> helper, redownloaded default

--- Lab01/defaultcode.rkt
+++ Lab01/defaultcode.rkt
@@ -1,85 +1,70 @@
 #lang racket
 
-; Domyślne struktury
-(provide 
-  (struct-out column-info)
-  (struct-out table)
-  (struct-out and-f)
-  (struct-out or-f)
-  (struct-out not-f)
-  (struct-out eq-f)
-  (struct-out eq2-f)
-  (struct-out lt-f)
-  table-insert
-  table-project
-  table-sort
-  table-select
-  table-rename
-  table-cross-join
-  table-natural-join
-)
+(provide (struct-out column-info)
+         (struct-out table)
+         (struct-out and-f)
+         (struct-out or-f)
+         (struct-out not-f)
+         (struct-out eq-f)
+         (struct-out eq2-f)
+         (struct-out lt-f)
+         table-insert
+         table-project
+         table-sort
+         table-select
+         table-rename
+         table-cross-join
+         table-natural-join)
 
-; Definicja column-info
 (define-struct column-info (name type) #:transparent)
 
-; Definicja table
 (define-struct table (schema rows) #:transparent)
 
-; Tabela cities
 (define cities
   (table
-    (list 
-      (column-info 'city    'string)
-      (column-info 'country 'string)
-      (column-info 'area    'number)
-      (column-info 'capital 'boolean)
-    )
-    (list 
-      (list "Wrocław" "Poland"  293 #f)
-      (list "Warsaw"  "Poland"  517 #t)
-      (list "Poznań"  "Poland"  262 #f)
-      (list "Berlin"  "Germany" 892 #t)
-      (list "Munich"  "Germany" 310 #f)
-      (list "Paris"   "France"  105 #t)
-      (list "Rennes"  "France"   50 #f)
-    )
-  )
-)
+   (list (column-info 'city    'string)
+         (column-info 'country 'string)
+         (column-info 'area    'number)
+         (column-info 'capital 'boolean))
+   (list (list "Wrocław" "Poland"  293 #f)
+         (list "Warsaw"  "Poland"  517 #t)
+         (list "Poznań"  "Poland"  262 #f)
+         (list "Berlin"  "Germany" 892 #t)
+         (list "Munich"  "Germany" 310 #f)
+         (list "Paris"   "France"  105 #t)
+         (list "Rennes"  "France"   50 #f))))
 
-; Tabela countries
 (define countries
   (table
-    (list 
-      (column-info 'country 'string)
-      (column-info 'population 'number)
-    )
-   (list 
-    (list "Poland" 38)
-    (list "Germany" 83)
-    (list "France" 67)
-    (list "Spain" 47))
-  )
-)
+   (list (column-info 'country 'string)
+         (column-info 'population 'number))
+   (list (list "Poland" 38)
+         (list "Germany" 83)
+         (list "France" 67)
+         (list "Spain" 47))))
 
-; Pusta tabela columns
 (define (empty-table columns) (table columns '()))
 
 ; Wstawianie
+
 (define (table-insert row tab)
   ;; uzupełnij
-)
+  )
 
 ; Projekcja
+
 (define (table-project cols tab)
   ;; uzupełnij
-)
+  )
 
 ; Sortowanie
+
 (define (table-sort cols tab)
   ;; uzupełnij
-)
+  )
 
 ; Selekcja
+
 (define-struct and-f (l r))
 (define-struct or-f (l r))
 (define-struct not-f (e))
@@ -89,19 +74,22 @@
 
 (define (table-select form tab)
   ;; uzupełnij
-)
+  )
 
 ; Zmiana nazwy
+
 (define (table-rename col ncol tab)
   ;; uzupełnij
-)
+  )
 
 ; Złączenie kartezjańskie
+
 (define (table-cross-join tab1 tab2)
   ;; uzupełnij
-)
+  )
 
 ; Złączenie
+
 (define (table-natural-join tab1 tab2)
   ;; uzupełnij
-)
+  )
