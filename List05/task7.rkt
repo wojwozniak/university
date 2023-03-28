e0ae5f7dbffb87e6cf37cc9cd6b6cb25c4b08bdf
Wojtek <pulchnymamut@gmail.com>
2023-03-28 10:15:33 +0200

    a

---
+++ List05/task7.rkt
@@ -0,0 +1,62 @@
+#lang plait
+
+
+(define-type Prop
+(var [v : String])
+(conj [l : Prop] [r : Prop])
+(disj [l : Prop] [r : Prop])
+(neg [f : Prop]) )
+
+
+
+
+
+(define prop1
+  (var "x"))
+
+(define prop2
+  (var "y"))
+
+(define prop3
+  (conj prop1 prop2))
+
+(define prop4 (disj prop3 prop1))
+
+
+
+
+
+;usuwanie duplikatow
+(define (rm-dupl lst)
+  (cond [(equal? '() lst) '()]
+    [(member (first lst) (rest lst))
+     (rm-dupl (rest lst))]
+    [else (cons (first lst)
+           (rm-dupl (rest lst)))]))
+
+
+
+; iteracja
+(define (iter x xs)
+   (cond [(neg? x)   (iter (neg-f x) xs)]
+          [(disj? x)  (iter (disj-r x) (iter (disj-l x) xs))]
+          [(conj? x)  (iter (conj-r x) (iter (conj-l x) xs))]
+          [(var? x) (append xs (list x))]))
+
+
+
+;wynik
+(define (free-vars x)
+     (rm-dupl (iter x (list))))
+
+(free-vars prop4)
+
+
+
+
+
+
+
+
+
+
---
+++ List05/task8.rkt
@@ -0,0 +1,45 @@
+#lang plait
+
+
+(define-type Prop
+(var [v : String])
+(conj [l : Prop] [r : Prop])
+(disj [l : Prop] [r : Prop])
+(neg [f : Prop]) )
+
+
+
+
+
+(define prop1
+  (var "x"))
+
+(define prop2
+  (var "y"))
+
+(define prop3
+  (conj prop1 prop2))
+
+(define prop4 (disj prop3 prop1))
+
+(define prop5
+  (disj prop1 prop2))
+
+
+(define ht (make-hash (list (pair "x" #f) (pair "y" #t))))
+
+;(some-v (hash-ref ht "x"))
+
+
+
+
+(define (iter f)
+   (cond [(neg? f)   (not (iter (neg-f f)))]
+          [(disj? f)  (or (iter (disj-r f)) (iter (disj-l f)))]
+          [(conj? f)  (and (iter (conj-r f)) (iter (conj-l f)))]
+          [else (some-v (hash-ref ht (var-v f)))]))
+
+
+
+(iter (conj prop5 prop1))
+
