97d8951a6be104214432ce485147f3535729ea62
Wojtek <pulchnymamut@gmail.com>
2023-04-03 16:38:49 +0200

    Update task8.rkt

--- List06/task8.rkt
+++ List06/task8.rkt
@@ -1,9 +1,19 @@
 #lang plait
 
+; Definicja funkcji sorted? 
 (define (sorted? xs)
     (cond 
-        [(empty? xs) #t]
-        [((first xs) > (second xs)) #f]
+        [(empty? (rest xs)) #t]
+        [(> (first xs) (first(rest xs))) #f]
         [else (sorted? (rest xs))]
+    )
+)
+
+; Definicja funkcji insert
+(define (insert x xs)
+    (cond 
+        [(empty? xs) (cons x (list))]
+        [(> (first xs) x) (cons x xs)]
+        [else (cons (first xs) (insert x (rest xs)))]
     )
 )
