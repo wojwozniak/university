0501cbbe0f88c8c7db260c1af18347060c3b75f8
Wojtek <pulchnymamut@gmail.com>
2023-04-17 22:05:14 +0200

    Create task4.rkt

---
+++ List07/task4.rkt
@@ -0,0 +1,41 @@
+#lang racket
+
+; (parametric- >/c [a b] (-> a b a))
+;   Kontrakt jest parametryczny, co oznacza, że działa dla dowolnych typów danych.
+;   Funkcja przyjmuje trzy argumenty, dwa o typie "a" i jeden o typie "b".
+;   Funkcja zwraca wartość o typie "a".
+(define/contract (f1 x y)
+    (parametric->/c [a b] (-> a b a))
+    x
+)
+
+
+; (parametric- >/c [a b c] (-> (-> a b c) (-> a b) a c))
+;   Kontrakt jest parametryczny, co oznacza, że działa dla dowolnych typów danych.
+;   Funkcja przyjmuje trzy argumenty, funkcję o typie (-> a b c), funkcję o typie (-> a b) i dwa argumenty o typie "a" i "c".
+;   Funkcja zwraca wartość o typie "a".
+(define/contract (f2 f g x)
+    (parametric->/c [a b c] (-> (-> a b c) (-> a b) a c))
+    (f x (g x))
+)
+
+
+; (parametric- >/c [a b c] (-> (-> b c) (-> a b) (-> a c)))
+;   Kontrakt jest parametryczny, co oznacza, że działa dla dowolnych typów danych.
+;   Funkcja przyjmuje trzy argumenty, funkcję o typie (-> b c), funkcję o typie (-> a b) i funkcję o typie (-> a c).
+(define/contract (f3 f g)
+    (parametric->/c [a b c] (-> (-> b c) (-> a b) (-> a c)))
+    (lambda (x)
+        (f (g x))
+    )
+)
+
+
+; (parametric- >/c [a] (-> (-> (-> a a) a) a))
+;   Kontrakt jest parametryczny, co oznacza, że działa dla dowolnych typów danych.
+;   Funkcja przyjmuje jeden argument, funkcję o typie (-> (-> a a) a).
+;   Funkcja zwraca wartość o typie "a".
+(define/contract (f4 f)
+    (parametric->/c [a] (-> (-> (-> a a) a) a))
+    (f f)
+)
