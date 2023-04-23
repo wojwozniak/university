ba10214a9f2280e8282410fc363444c18d9c057b
Wojtek <pulchnymamut@gmail.com>
2023-04-23 18:23:07 +0200

    Create task1.rkt

---
+++ List08/task1.rkt
@@ -0,0 +1,22 @@
+#lang racket
+
+; Zadanie 1
+
+; Zdefiniuj procedurę cycle!, która zapętla listę mutowalną, 
+; czyli przepina wskaźnik ogona ostatniego elementu na początek listy.
+
+; Użyte funkcje:
+; last-pair - zwraca ostatni element listy (ten o cdr równym null) 
+; set-cdr! - mutuje listę, przepinając wskaźnik cdr podanej pary na drugi argument
+
+; Definicja procedury cycle! (Przyjmuje jedną listę jako argument) 
+(define (cycle! lst)
+    ; Zapisujemy resztę listy jako tail
+    (define tail (cdr lst))
+
+    ; Ustawiamy wskaźnik ostatniego elementu na początek listy
+    (set-cdr! (last-pair lst) lst)
+
+    ; Zwracamy tail (resztę listy)
+    tail
+)
