0ec9974c246c80cd060f8c3ecee0986b4ef3b8c0
Wojtek <pulchnymamut@gmail.com>
2023-04-23 19:04:17 +0200

    Update task3.rkt

--- List08/task3.rkt
+++ List08/task3.rkt
@@ -16,24 +16,24 @@
 
 ; Kontrakty
 (provide
-    mdqueue?
-    nonempty-mdqueue?
+    mqueue?
+    nonempty-mqueue?
     (contract-out
-        [mdqueue-empty?  (-> mdqueue? boolean?)]
-        [make-mdqueue    (-> mdqueue?)]
-        [mdqueue-push-front   (-> nonempty-mdqueue? any/c)]
-        [mdqueue-push-back   (-> nonempty-mdqueue? any/c)]
-        [mdqueue-pop-front    (-> (or nonempty-mdqueue? mdqueue?) any/c)]
-        [mdqueue-pop-back    (-> (or nonempty-mdqueue? mdqueue?) any/c)]
+        [mqueue-empty?  (-> mqueue? boolean?)]
+        [make-mqueue    (-> mqueue?)]
+        [mqueue-push-front   (-> nonempty-mqueue? any/c)]
+        [mqueue-push-back   (-> nonempty-mqueue? any/c)]
+        [mqueue-pop-front   (-> mqueue? any/c)]
+        [mqueue-pop-back    (-> mqueue? any/c)]
         ; Zostawiamy jednego join - jesteśmy w stanie uzyskać każdą 
         ; kolejność połączenia poprzez wywołanie join z odpowiednią kolejnością argumentów
-        [mdqueue-join    (-> nonempty-mdqueue? nonempty-mdqueue? void?)]
+        [mqueue-join    (-> nonempty-mqueue? nonempty-mqueue? void?)]
     )
 )
 
 
 ; Definicja kolejki dwukierunkowej
-(struct mdqueue
+(struct mqueue
     (
         [front #:mutable]
         [back  #:mutable]
@@ -42,35 +42,35 @@
 
 
 ; Procedura empty? dla kolejki dwukierunkowej
-(define (mdqueue-empty? q)
+(define (mqueue-empty? q)
     (and 
-        (null? (mdqueue-front q))
-        (null? (mdqueue-back  q))
+        (null? (mqueue-front q))
+        (null? (mqueue-back  q))
     )
 )
 
 
 ; Procedura nonempty? dla kolejki dwukierunkowej
-(define (nonempty-mdqueue? q)
+(define (nonempty-mqueue? q)
     (and 
-        (mdqueue? q) 
-        (mpair? (mdqueue-front q))
+        (mqueue? q) 
+        (mpair? (mqueue-front q))
     )
 )
 
 
 ; Procedura make dla kolejki dwukierunkowej
-(define (make-mdqueue)
-    (mdqueue null null)
+(define (make-mqueue)
+    (mqueue null null)
 )
 
 ; Procedura push front dla kolejki dwukierunkowej
 ; Przyjmuje kolejkę q i element x do wstawienia
-(define (mdqueue-push-front q x)
+(define (mqueue-push-front q x)
     ; Tworzymy nowy węzęł p z wartością x i dwoma wskaźnikami
     (define p (mcons x null null))
-    (if (mdqueue-empty? q)
-        ; Jeśli kolejka jest pusta to ustawiamy front i back na nowy węzeł
+    (if (mqueue-empty? q)
+        ; Jeśli kolejka jest pusta to ustawiamy front i back na nowy węzeł (begin używamy by wykonać kilka poleceń jako jeden blok w if)
         (begin
             (set-mqueue-front! q p)
             (set-mqueue-back! q p)
@@ -78,9 +78,9 @@
         ; w.p.p.
         (begin
             ; Ustawiamy cdr węzła p na węzęł front
-            (set-mcdr p (mdqueue-front q))
+            (set-mcdr p (mqueue-front q))
             ; Ustawiamy car dotychczasowego frontu kolejki q na węzeł p
-            (set-mcar (mdqueue-front q) p)
+            (set-mcar (mqueue-front q) p)
             ; Ustawiamy front na węzeł p
             (set-mqueue-front! q p)
         )
@@ -89,18 +89,31 @@
 
 ; Procedura push back dla kolejki dwukierunkowej
 ; Działa analogicznie do push front
-(define (mdqueue-push-back q x)
+(define (mqueue-push-back q x)
     (define p (mcons x null null))
-    (if (mdqueue-empty? q)
+    (if (mqueue-empty? q)
         (begin
             (set-mqueue-front! q p)
             (set-mqueue-back! q p)
         )
         (begin
-            (set-mcdr (mdqueue-back q) p)
-            (set-mcdr p (mdqueue-back q))
+            (set-mcdr (mqueue-back q) p)
+            (set-mcdr p (mqueue-back q))
             (set-mqueue-back! q p)
         )
     )
 )
 
+; Definicja procedury pop front dla kolejki dwukierunkowej
+(define (mqueue-pop-front q)
+    (define p (mqueue-front q))
+    (set-mqueue-front! q (mcdr p))
+    (if (null? (mcdr p))
+        (begin
+            (set-mqueue-back! q null)
+            (mcar p))
+        (begin
+            (set-mcar (mcdr p) null)
+            (mcar p))
+    )
+)
