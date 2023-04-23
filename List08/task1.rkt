d0c064b515204814249314e68c596ca175f82ab1
Wojtek <pulchnymamut@gmail.com>
2023-04-23 21:39:21 +0200

    Update task1.rkt

--- List08/task1.rkt
+++ List08/task1.rkt
@@ -6,15 +6,24 @@
 ; czyli przepina wskaźnik ogona ostatniego elementu na początek listy.
 
 
+; Pomocnicza funkcja helper, która zwraca ostatni element listy
+(define (helper lst)
+    (cond
+        [(null? (mcdr lst)) lst]
+        [(mpair? lst) (helper (mcdr lst))]
+        [else #f]
+    )
+)
+
 ; Definicja prodecury cycle!
 (define (cycle! lst)
-    (let ((llst
-            (cond
-                [(null? (mcdr lst)) lst]
-                [(mpair? lst) (cycle! (mcdr lst))]
-                [else #f]
-            )
-        ))
+    (let 
+        ((llst (helper lst))) 
         (set-mcdr! llst lst)
     )
 )
+
+(define lst (mcons 1 (mcons 2 (mcons 3 '()))))
+(displayln lst)
+(cycle! lst)
+(displayln lst)
