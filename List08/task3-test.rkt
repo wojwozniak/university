a63cc7bb5d980f6d62bd8e76614e604191420ad6
Wojtek <pulchnymamut@gmail.com>
2023-04-24 23:08:38 +0200

    Update task3-test.rkt

--- List08/task3-test.rkt
+++ List08/task3-test.rkt
@@ -7,7 +7,7 @@
 
 ; Sprawdzamy czy kolejka jest pusta
 (displayln "Sprawdzamy czy kolejka jest pusta:")
-(displayln (mqueue-empty? q)) ; oczekiwany output: #t
+(displayln (mqueue-empty? q)) ; Oczekiwany output: #t
 
 ; Dodajemy elementy do kolejki
 (displayln "Dodajemy elementy do kolejki:")
@@ -18,8 +18,17 @@
 
 ; Sprawdzamy czy kolejka jest pusta
 (displayln "Sprawdzamy czy kolejka jest pusta:")
-(displayln (mqueue-empty? q)) ; oczekiwany output: #f
+(displayln (mqueue-empty? q)) ; Oczekiwany output: #f
 
 ; Wypisujemy zawartość kolejki
-(displayln "Wypisujemy zawartość kolejki:")
-(mqueue-print q)
+;(displayln "Wypisujemy zawartość kolejki:")
+;(mqueue-print q) ; Oczekiwany output: {4 2 1 3}
+
+; Usuwamy elementy z kolejki
+(displayln "Usuwamy elementy z kolejki:")
+(mqueue-pop-front q)
+(mqueue-pop-back q)
+
+; Wypisujemy zawartość kolejki
+;(displayln "Wypisujemy zawartość kolejki:")
+;(mqueue-print q) ; Oczekiwany output: {2 1}
