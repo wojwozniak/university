71ebe793b22e61c92763c9ef6ae49232e034f980
Wojtek <pulchnymamut@gmail.com>
2023-04-24 14:16:07 +0200

    styling

--- List08/task5-lecturecode.rkt
+++ List08/task5-lecturecode.rkt
@@ -10,36 +10,40 @@
 )
 
 (define (parse-Op s)
-  (let ([sym (s-exp->symbol s)])
-  (cond
-    [(equal? sym '+) (op-add)]
-    [(equal? sym '-) (op-sub)]
-    [(equal? sym '*) (op-mul)]
-    [(equal? sym '/) (op-div)])))
+    (let ([sym (s-exp->symbol s)])
+        (cond
+            [(equal? sym '+) (op-add)]
+            [(equal? sym '-) (op-sub)]
+            [(equal? sym '*) (op-mul)]
+            [(equal? sym '/) (op-div)]
+        )
+    )
+)
 
 (define (parse-Exp s)
-  (cond
-    [(s-exp-number? s) (exp-number (s-exp->number s))]
-    [(s-exp-list? s)
-        (let 
-            ([xs (s-exp->list s)])
-            (exp-op (parse-Op  (first  xs))
-                (parse-Exp (second xs))
-                (parse-Exp (third  xs))
+    (cond
+        [(s-exp-number? s) (exp-number (s-exp->number s))]
+        [(s-exp-list? s)
+            (let 
+                ([xs (s-exp->list s)])
+                (exp-op (parse-Op  (first  xs))
+                    (parse-Exp (second xs))
+                    (parse-Exp (third  xs))
+                )
             )
-        )
-    ]
+        ]
     )
 )
 
 ; ==============================================
 
 (define (eval-op op)
-  (type-case Op op
-    [(op-add) +]
-    [(op-sub) -]
-    [(op-mul) *]
-    [(op-div) /])
+    (type-case Op op
+        [(op-add) +]
+        [(op-sub) -]
+        [(op-mul) *]
+        [(op-div) /]
+    )
 )
 
 (define (eval e)
