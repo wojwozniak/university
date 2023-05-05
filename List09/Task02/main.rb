97e0f7591321d3ec974add6437ad74a586a565a1
Wojtek <pulchnymamut@gmail.com>
2023-05-05 20:16:55 +0200

    Update main.rb

--- List09/Task02/main.rb
+++ List09/Task02/main.rb
@@ -19,29 +19,7 @@ class Function2D
     
     # Metoda volume - oblicza przybliżoną objętość między wykresem funkcji a leżącym na płaszczyźnie OXOY prostokątem [a, b] ×[c, d]
     def volume(a, b, c, d)
-        # Ilość podziałów prostokąta
-        n = 1000
-        # Szerokość i wysokość podprzedziału
-        width = (b - a).to_f / n
-        height = (d - c).to_f / n
-        # Suma objętości podprzedziałów
-        sum = 0.0
-        # Wartość funkcji w punkcie (a, c)
-        x = a
-        y = c
-  
-      # Obliczamy sumę objętości podprzedziałów
-      n.times do
-            m.times do
-                volume = @func.call(x, y) * width * height
-                sum += volume
-                y += height
-            end
-        x += width
-      end
-  
-        # Zwracamy sumę objętości podprzedziałów
-        return sum
+        
     end
     
     # Metoda contour_line
@@ -86,3 +64,13 @@ puts f.value(0, 0)
 puts "Obliczenie wartości funkcji w punkcie (1,2)"
 puts "Oczekiwany output: '5'"
 puts f.value(1, 2)
+
+
+# Obliczenie objętości figury między powierzchnią a płaszczyzną xy dla z=1/2
+
+
+
+# Obliczenie linii poziomicy dla wartości z=1/2 w przedziale x: 0 do 1 i y: 0 do 1
+puts "Obliczenie linii poziomicy dla wartości z=1/2 w przedziale x: 0 do 1 i y: 0 do 1"
+puts "Oczekiwany output: [[0.707106781186548,0.707106781186548],[0.700000000000001,0.7141421356237308], [0.6931972057930319,0.7213203435596425], [0.686592294034003,0.7286403486601181], [0.6801804793453329,0.7361018038650837], [0.6739572383563089,0.7437047620909091], [0.6679183427733793,0.7514495763074173], [0.6620598451114869,0.7593369471878772], [0.6563770871643722,0.7673678821226834], [0.6508656671843195,0.7755437933489398] ..."
+# puts f.contour_line(0, 1, 0, 1, 1, 0.01)
