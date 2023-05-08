026ec9516f7af4d3ffcff963ddb888f874eadf31
Wojtek <pulchnymamut@gmail.com>
2023-05-08 15:00:44 +0200

    Create expparser.rkt

---
+++ List09/task3/expparser.rkt
@@ -0,0 +1,32 @@
+#lang racket
+(require "parsing.rkt")
+
+(define op-parser
+  `((+ ,op-add)
+    (- ,op-sub)
+    (* ,op-mul)
+    (/ ,op-div))
+)
+
+(define exp-parser
+    `(
+        (NUMBER ,exp-number)
+        (
+            (SYMBOL ANY ANY) 
+            ,(lambda 
+                (op e1 e2)
+                (exp-op 
+                    (parse-op op)
+                    (parse-exp e1)
+                    (parse-exp e2)
+                )
+            )
+        )
+    )
+)
+
+(define (parse-op s) (run-parser op-parser s))
+
+(define (parse-exp s) (run-parser exp-parser s))
+
+(provide parse-op parse-exp)
