daf482604364cbeaabb7d781cc9f70149a694dfd
Wojtek <pulchnymamut@gmail.com>
2023-05-14 20:38:56 +0200

    delete useless texts

--- List10/Task1/arith.rkt
+++ List10/Task1/arith.rkt
@@ -43,42 +43,8 @@
     [else (error 'parse "unknown operator")]
   )
 )
-                 
-(module+ test
-  (test 
-    (parse `2)
-    (numE 2)
-  )
-  (test 
-    (parse `{+ 2 1})
-    (opE (add) (numE 2) (numE 1))
-  )
-  (test 
-    (parse `{* 3 4})
-    (opE (mul) (numE 3) (numE 4))
-  )
-  (test 
-    (parse `{+ {* 3 4} 8})
-    (opE 
-      (add)
-      (opE (mul) (numE 3) (numE 4))
-      (numE 8)
-    )
-  )
-  (test/exn 
-    (parse `{{+ 1 2}})
-    "invalid input"
-  )
-  (test/exn 
-    (parse `{+ 1})
-    "invalid input"
-  )
-  (test/exn 
-    (parse `{^ 1 2})
-    "unknown operator"
-  )
-)
-  
+
+
 ;; eval --------------------------------------
 
 (define-type-alias Value Number)
@@ -104,16 +70,6 @@
   (eval (parse e))
 )
 
-(module+ test
-  (test (run `2)
-        2)
-  (test (run `{+ 2 1})
-        3)
-  (test (run `{* 2 1})
-        2)
-  (test (run `{+ {* 2 3} {+ 5 8}})
-        19)
-)
 
 ;; printer ———————————————————————————————————-
 
