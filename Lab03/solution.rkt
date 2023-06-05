55b4fa5ff52918e06eb3ad022b450f9ab20d2415
Wojtek <pulchnymamut@gmail.com>
2023-06-05 20:06:21 +0200

    update

--- Lab03/solution.rkt
+++ Lab03/solution.rkt
@@ -2,6 +2,13 @@
 
 (define-type-alias Value Number)
 
+(define-type-alias S-Exp (U Number (Listof S-Exp)))
+
+; parse
+(define (parse [s : String]) : S-Exp
+  (error 'parse "not implemented")
+)
+
 (define (run [s : S-Exp]) : Value
   (error 'run "not implemented")
 )
