b72e643fd9a1e9d491fed8248f562c3429828d35
Wojtek <pulchnymamut@gmail.com>
2023-06-21 22:32:48 +0200

    Update zipunzip.rkt

--- ExamPrep/Tasks/zipunzip.rkt
+++ ExamPrep/Tasks/zipunzip.rkt
@@ -32,5 +32,27 @@
 )
 
 ; Test unzip
+;(unzip (zip '(1 2 3) '(4 5 6)))
 
-(unzip (zip '(1 2 3) '(4 5 6)))
+
+(define (even_pos lst)
+    (define (helper lst even)
+        (cond
+            [(null? lst) even]
+            [(null? (cdr lst)) even]
+            [else
+                (helper (cddr lst) (cons (cadr lst) even))
+            ]
+        )
+    )
+    (reverse (helper lst '()))
+)
+
+; Całkiem prosta idea
+; Deklarujemy wewnętrzną pomocniczą funkcję przechowującą output jako arg
+; Jeśli lista ma 0,1 el. to zwracamy output
+; w.p.p. wywołujemy rekurencyjnie helper na cddr listy i consujemy cadr listy do outputu
+
+
+; Test even_pos
+(even_pos '(1 2 3 4 5))
