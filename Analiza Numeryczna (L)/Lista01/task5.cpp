96805570d5df43f0423152ad9e59754dbe32a3ce
Wojtek <pulchnymamut@gmail.com>
2023-10-08 22:18:36 +0200

    Update task5.cpp

--- Analiza Numeryczna (L)/Lista01/task5.cpp
+++ Analiza Numeryczna (L)/Lista01/task5.cpp
@@ -1,19 +1,63 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
+#include <functional>
+#include <numeric>
 
-double calculate(double x) {
-    double result;
+using namespace std;
 
-    // sqrt( x^14 + 1 )
-    double squareRoot = sqrt(pow(x, 14) + 1);
 
-    // (squareRoot - 1) / (x^14)
-    result = (squareRoot - 1) / pow(x, 14);
+double calculateIn(int n) {
+    if (n == 0) {
+        double out = log(2024 / 2023);
+        return out; // I0 = ln(2024 / 2023)
+    }
 
-    return 4046 * result;
+    double In_1 = calculateIn(n - 1);
+    return (1.0 / n) - 2023 * In_1;
 }
 
+
+double f(double x, int n) {
+    return pow(x, n) / (x + 2023);
+}
+
+double calculateInIntegral(int n) {
+    double dx = 0.001;
+    double suma = 0.0;
+    for (double x = 0; x <= 1; x += dx) {
+        suma += f(x, n) * dx;
+    }
+    return suma;
+}
+
+
 int main() {
-    printf("Result: %lf\n", calculate(0.001));
+    vector<double> oddResults; // Przechowuje wyniki dla I1, I3, ..., I19
+    vector<double> evenResults; // Przechowuje wyniki dla I2, I4, ..., I20
+    vector<double> integralResults; // Przechowuje wyniki dla I1, I2, I3, ..., I20 z całki
+
+    for (int n = 1; n <= 20; ++n) {
+        double In_result = calculateIn(n);
+        if (n % 2 == 1) {
+            oddResults.push_back(In_result);
+        } else {
+            evenResults.push_back(In_result);
+        }
+        integralResults.push_back(calculateInIntegral(n));
+    }
+
+    cout << "Wyniki dla podciagu I1, I3, ..., I19:" << endl;
+    for (int i = 0; i < oddResults.size(); ++i) {
+        cout << "I" << 2 * i + 1 << "         = " << oddResults[i] << endl;
+        cout << "I" << 2 * i + 1 << " z calki = " << integralResults[i] << endl;
+    }
+
+    cout << "Wyniki dla podciagu I2, I4, ..., I20:" << endl;
+    for (int i = 0; i < evenResults.size(); ++i) {
+        cout << "I" << 2 * i + 2 << "         = " << evenResults[i] << endl;
+        cout << "I" << 2 * i + 2 << " z calki = " << integralResults[i] << endl;
+    }
+    
     return 0;
 }
