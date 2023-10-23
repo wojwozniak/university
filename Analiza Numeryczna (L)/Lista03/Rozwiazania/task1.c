1840f76c1c6f15044b7944489714565b04eb4067
Wojtek <pulchnymamut@gmail.com>
2023-10-23 16:22:06 +0200

    Update task1.c

--- Analiza Numeryczna (L)/Lista03/Rozwiazania/task1.c
+++ Analiza Numeryczna (L)/Lista03/Rozwiazania/task1.c
@@ -28,20 +28,28 @@ double calculate_b2(double x) {
 */
 
 double calculate_b1(double x) {
-    return log2(x) / log2(5) - 6;
+    return log2(x) - 2;
 }
 
 double calculate_b2(double x) {
-    double num = x / 15625;
-    return log2(num) / log(5);
+    double num = x / 4.0;
+    return log2(num);
 }
 
+double calculate_c1(double x) {
+
+}
 
 
 int main() {
     //printf("ppkt a test 1 dla x = 0.001: %f\n", calculate_a1(-6200));
     //printf("ppkt a test 2 dla x = 0.001: %f\n", calculate_a2(-6200));
-    printf("ppkt b test 1 dla x = 4: %f\n", calculate_b1(15624.9));
-    printf("ppkt b test 2 dla x = 4: %f\n", calculate_b2(15624.9));
+    for(int i=0; i<100; i++) {
+        double x = 4 + pow(10, -i);
+        printf("ppkt b test 1 dla x = 4: %LE %LE \n", calculate_b1(x), calculate_b2(x));
+        //printf("ppkt b test 2 dla x = 4: %f\n", calculate_b2(x));
+    }
     return 0;
 }
+
+// 4 * 10^-15
