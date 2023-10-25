baf60a5d038b786f9080d29b3f8f0f2042a0ff03
Wojtek <pulchnymamut@gmail.com>
2023-10-26 00:09:28 +0200

    jednak publiczne zeby nie pisac miliona getterow

--- List03/figury/Punkt.java
+++ List03/figury/Punkt.java
@@ -1,8 +1,8 @@
 package List03.figury;
 
 public class Punkt {
-    private double x;
-    private double y;
+    public double x;
+    public double y;
 
     public Punkt(double x, double y) {
         this.x = x;
