b88cda2aae062975a6e564d24149712e64df7c96
Wojtek <pulchnymamut@gmail.com>
2023-10-25 22:52:09 +0200

    Create Punkt.java

---
+++ List03/figury/Punkt.java
@@ -0,0 +1,11 @@
+package List03.figury;
+
+public class Punkt {
+    private double x;
+    private double y;
+
+    public Punkt(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
