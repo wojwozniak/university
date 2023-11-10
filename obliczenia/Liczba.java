145139b4c922400837024193da6bdf15cecfe283
Wojtek <pulchnymamut@gmail.com>
2023-11-11 00:17:40 +0100

    Create Liczba.java

---
+++ obliczenia/Liczba.java
@@ -0,0 +1,31 @@
+package obliczenia;
+
+public class Liczba extends Wyrazenie {
+    private double wartosc;
+
+    public Liczba(double wartosc) {
+        this.wartosc = wartosc;
+    }
+
+    @Override
+    public double oblicz() {
+        return (int) wartosc;
+    }
+
+    @Override
+    public String toString() {
+        return Double.toString(wartosc);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Liczba)) {
+            return false;
+        }
+        Liczba other = (Liczba) obj;
+        return wartosc == other.wartosc;
+    }
+}
