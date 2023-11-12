cfa1b9c590a019f0939e7f4ab2c461a0c5e23b15
Wojtek <pulchnymamut@gmail.com>
2023-11-12 13:00:47 +0100

    Create Sinus.java

---
+++ obliczenia/dzialania/Sinus.java
@@ -0,0 +1,16 @@
+package obliczenia.dzialania;
+
+import obliczenia.Wyrazenie;
+
+public class Sinus extends Wyrazenie {
+    private final Wyrazenie wyr;
+
+    public Sinus(Wyrazenie wyr) {
+        this.wyr = wyr;
+    }
+
+    @Override
+    public double oblicz() {
+        return Math.sin(wyr.oblicz());
+    }
+}
