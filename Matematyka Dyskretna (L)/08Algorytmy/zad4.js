c40a18f5234228610fe17b0313ceba418567142e
Wojtek <pulchnymamut@gmail.com>
2023-11-27 18:26:11 +0100

    Update zad4.js

--- Matematyka Dyskretna (L)/08Algorytmy/zad4.js
+++ Matematyka Dyskretna (L)/08Algorytmy/zad4.js
@@ -6,16 +6,52 @@
 const czyRowne = (G, H) => {
     if (G.length !== H.length) return false; // Sprawdzamy czy warunek na pewno jest spełniony
     const n = G.length; // Liczba wierzchołków
-    const odwiedzone = new Array(G.length).fill(false); // Tablica odwiedzonych wierzchołków
-    let diff = 0; // Różnica między stopniami wierzchołków
-    for (krawedz in G) {
-        odwiedzone[krawedz] = true; // Oznaczamy wierzchołek jako odwiedzony
-        ++diff;
+    let vertices = []; // Tablica wierzchołków
+    for (let i = 0; i < n; i++) vertices.push(i); // Dodajemy wierzchołki do tablicy
+    let visited; // Tablica odwiedzonych wierzchołków
+    let node_diff; // Różnica wierzchołków
+
+    for(let i=0; i<n; ++i) {
+        visited = new Array(n).fill(false); // Czyścimy tablicę odwiedzonych wierzchołków
+        node_diff = 0; // Zerujemy różnicę wierzchołków
+        for(vertex in G[i]) {
+            visited[vertex] = true; // Oznaczamy wierzchołek jako odwiedzony
+            ++node_diff; // Zwiększamy różnicę wierzchołków
+        }
+        for(vertex in H[i]) {
+            if(visited[vertex]) --node_diff; // Jeśli wierzchołek jest odwiedzony to zmniejszamy różnicę
+            else return false; // W przeciwnym wypadku zwracamy false
+        }
+        if(node_diff !== 0) return false; // Jeśli różnica wierzchołków jest różna od 0 to zwracamy false
     }
-    for (krawedz in H) {
-        if (!odwiedzone[krawedz]) return false; // Jeśli wierzchołek nie był odwiedzony, to grafy nie są izomorficzne
-        --diff;
-    }
-    if (diff !== 0) return false; // Różna ilość krawędzi - grafy nie są izomorficzne
-    return true;
+    return true; // Zwracamy true jeśli wszystkie warunki są spełnione
 }
+
+
+const G = [
+    [1, 2],
+    [0, 2],
+    [0, 1]
+];
+const H = [
+    [1, 2],
+    [0, 2],
+    [0, 1]
+];
+const wierzcholki = [0, 1, 2];
+console.log(czyRowne(G, H, wierzcholki)); // true
+
+
+const G2 = [
+    [1, 2],
+    [0, 2],
+    [0, 1]
+];
+const H2 = [
+    [1, 2],
+    [0, 2],
+    [0, 1, 2]
+];
+const wierzcholki2 = [0, 1, 2];
+
+console.log(czyRowne(G2, H2, wierzcholki2)); // false
