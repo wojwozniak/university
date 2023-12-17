75e67107e1aeff0354213d649660864c50041873
Wojtek <pulchnymamut@gmail.com>
2023-12-17 12:31:27 +0100

    Create zad1.js

---
+++ Matematyka Dyskretna (L)/11Algorytmy/zad1.js
@@ -0,0 +1,33 @@
+/**
+ * Sortowanie topologiczne w złożoności czasowej O(V + E)
+ * @param {[][]} graph - graf w postaci listy sąsiedztwa
+ * @param {[]} queue - kolejka wierzchołków 0 stopnia
+ * @returns {[]} - posortowana topologicznie kolejność wierzchołków
+ * lub null jeśli graf zawiera cykl (ale z założenia zadania nie zawiera)
+ */
+const topoSort = (graph, queue) => {
+    const R = []; // Lista wynikowa
+    let Q = [...queue]; // Kopia kolejki
+
+    // Dopóki kolejka nie jest pusta
+    while (Q.length > 0) {
+        const v = Q.shift(); // Usuń pierwszy element z kolejki i dodaj do listy wynikowej
+        R.push(v);
+
+        // Dla każdego sąsiada wierzchołka v (krawędź v -> u)
+        for (const u of graph[v]) {
+            // Usuń krawędź v -> u
+            graph[v] = graph[v].filter(x => x !== u);
+
+            // Jeśli u po usunięciu krawędzi v -> u nie ma już żadnych krawędzi wchodzących
+            if (graph.every(x => !x.includes(u))) {
+                Q.push(u); // Dodaj u do kolejki
+            }
+        }
+    }
+
+    if(graph.some(x => x.length > 0)) return null;
+
+
+    return result;
+}
