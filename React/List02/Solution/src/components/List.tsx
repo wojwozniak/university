5590da138b17bc4342434f54c1c4f7572363c19b
Wojtek <pulchnymamut@gmail.com>
2024-03-09 16:37:51 +0100

    wyczyszczenie warningow

--- React/List02/Solution/src/components/List.tsx
+++ React/List02/Solution/src/components/List.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Task } from '../interfaces/task'
 import SingleTask from './SingleTask'
 
--- React/List02/Solution/src/components/SingleTask.tsx
+++ React/List02/Solution/src/components/SingleTask.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { Task } from '../interfaces/task'
 
 interface SingleTaskProps {
