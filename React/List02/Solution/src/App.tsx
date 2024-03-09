53616454692f675d78e8f1410c5c21d89cf91bb7
Wojtek <pulchnymamut@gmail.com>
2024-03-09 17:31:27 +0100

    move navigation out

--- React/List02/Solution/src/App.tsx
+++ React/List02/Solution/src/App.tsx
@@ -4,6 +4,7 @@ import List from "./components/List"
 import { Task } from "./interfaces/task"
 import Popup from "./components/Popup"
 import BottomBar from "./components/BottomBar"
+import Navigation from "./components/Navigation"
 
 const defaultTasks: Task[] = [
   {
@@ -62,29 +63,14 @@ function App() {
 
   return (
     <div id="root" className="m-2">
-      <nav className="m-2 fixed right-0 top-0 flex flex-row items-center gap-5">
-        <button className="p-2 bg-blue-500 hover:bg-blue-700 rounded-lg text-white"
-          onClick={() => setSearchButtonClicked(Date.now())}
-        >
-          Search
-        </button>
-        <input type="text"
-          className="p-2 border"
-          placeholder="Text to search for"
-          content={searchText}
-          onChange={e => setSearchText(e.target.value)} />
-        <h2 className="text-xl">Displaying {filterOutFinished ? "active" : "all"} items</h2>
-        <button className="p-2 bg-yellow-500 hover:bg-yellow-700 rounded-lg text-white"
-          onClick={() => setFilterOutFinished(!filterOutFinished)}
-        >
-          {filterOutFinished ? "Show all" : "Show only active"}
-        </button>
-        <button className="p-2 bg-green-500 hover:bg-green-700 rounded-lg text-white"
-          onClick={() => setIsPopupOpen(!isPopupOpen)}
-        >
-          Add Task
-        </button>
-      </nav>
+      <Navigation searchText={searchText}
+        setSearchText={setSearchText}
+        filterOutFinished={filterOutFinished}
+        isPopupOpen={isPopupOpen}
+        setFilterOutFinished={setFilterOutFinished}
+        setSearchButtonClicked={setSearchButtonClicked}
+        setIsPopupOpen={setIsPopupOpen}
+      />
       <Popup render={isPopupOpen}
         updateRender={setIsPopupOpen}
         component={<AddItemPopup addTask={addTask} updateRender={setIsPopupOpen} />} />
--- React/List02/Solution/src/components/Navigation.tsx
+++ React/List02/Solution/src/components/Navigation.tsx
@@ -1,8 +1,48 @@
 import React from 'react'
 
-const Navigation = () => {
+interface NavigationProps {
+  searchText: string,
+  setSearchText: React.Dispatch<React.SetStateAction<string>>,
+  setSearchButtonClicked: React.Dispatch<React.SetStateAction<number>>,
+  setFilterOutFinished: React.Dispatch<React.SetStateAction<boolean>>,
+  filterOutFinished: boolean,
+  isPopupOpen: boolean,
+  setIsPopupOpen: React.Dispatch<React.SetStateAction<boolean>>,
+}
+
+const Navigation = ({
+  searchText,
+  setSearchText,
+  setSearchButtonClicked,
+  setFilterOutFinished,
+  filterOutFinished,
+  isPopupOpen,
+  setIsPopupOpen
+}: NavigationProps) => {
   return (
-    <div>Navigation</div>
+    <nav className="m-2 fixed right-0 top-0 flex flex-row items-center gap-5">
+      <button className="p-2 bg-blue-500 hover:bg-blue-700 rounded-lg text-white"
+        onClick={() => setSearchButtonClicked(Date.now())}
+      >
+        Search
+      </button>
+      <input type="text"
+        className="p-2 border"
+        placeholder="Text to search for"
+        content={searchText}
+        onChange={e => setSearchText(e.target.value)} />
+      <h2 className="text-xl">Displaying {filterOutFinished ? "active" : "all"} items</h2>
+      <button className="p-2 bg-yellow-500 hover:bg-yellow-700 rounded-lg text-white"
+        onClick={() => setFilterOutFinished(!filterOutFinished)}
+      >
+        {filterOutFinished ? "Show all" : "Show only active"}
+      </button>
+      <button className="p-2 bg-green-500 hover:bg-green-700 rounded-lg text-white"
+        onClick={() => setIsPopupOpen(!isPopupOpen)}
+      >
+        Add Task
+      </button>
+    </nav>
   )
 }
 
