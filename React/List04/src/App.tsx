6d3e3eb219781251cd96d7aed34a97a93b275b6b
Wojtek <pulchnymamut@gmail.com>
2024-03-15 20:00:37 +0100

    use context

--- React/List04/src/App.tsx
+++ React/List04/src/App.tsx
@@ -1,12 +1,15 @@
+import { RecipeProvider } from "./RecipeContext"
 import Navbar from "./components/Navbar"
 import RecipeList from "./components/RecipeList"
 
 function App() {
   return (
-    <div id="root">
-      <Navbar />
-      <RecipeList />
-    </div>
+    <RecipeProvider>
+      <div id="root">
+        <Navbar />
+        <RecipeList />
+      </div>
+    </RecipeProvider>
   )
 }
 
