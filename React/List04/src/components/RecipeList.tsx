9a36f6141336863661ac8c6e14e805c70f2b2627
Wojtek <pulchnymamut@gmail.com>
2024-03-15 20:00:57 +0100

    render list correctly

--- React/List04/src/components/RecipeList.tsx
+++ React/List04/src/components/RecipeList.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { RecipeContext } from '../RecipeContext';
+import SingleRecipe from './SingleRecipe';
 
 const RecipeList = () => {
+  const { state } = useContext(RecipeContext);
+
   return (
-    <div>RecipeList</div>
+    <main id="list" className='w-3/5 p-2'>
+      {state.recipes.map((recipe, index) => <SingleRecipe key={index} recipe={recipe} />)}
+    </main>
   )
 }
 
