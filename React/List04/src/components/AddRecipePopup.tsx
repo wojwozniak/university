b024caa751f595c35a03a4286f3bbf480e0c69d7
Wojtek <pulchnymamut@gmail.com>
2024-03-15 20:18:35 +0100

    addrecipe works

--- React/List04/src/components/AddRecipePopup.tsx
+++ React/List04/src/components/AddRecipePopup.tsx
@@ -1,8 +1,53 @@
-import React, { SetStateAction } from 'react'
+import React, { useContext } from 'react'
+import { RecipeContext } from '../RecipeContext';
 
-const AddRecipePopup = ({ updateRender }: { updateRender: React.Dispatch<SetStateAction<boolean>> }) => {
+interface AddRecipePopupProps {
+  setIsPopupOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const AddRecipePopup: React.FC<AddRecipePopupProps> = ({ setIsPopupOpen }) => {
+  const { dispatch } = useContext(RecipeContext);
+  const [recipeName, setRecipeName] = React.useState<string>('');
+  const [recipeDesc, setRecipeDesc] = React.useState<string>('');
+
+  const addRecipe = (name: string, description: string) => {
+    if (!name || !description) return;
+    dispatch({
+      type: 'ADD_RECIPE',
+      recipe: {
+        id: Date.now(),
+        name,
+        description,
+      },
+    });
+  };
+
+  const addTaskClick = () => {
+    if (!recipeName || !recipeDesc) return;
+    addRecipe(recipeName, recipeDesc);
+    setIsPopupOpen(false);
+  };
+
   return (
-    <div>AddRecipePopup</div>
+    <div className="text-black">
+      <h1 className="text-2xl">Add Task</h1>
+      <div className='flex flex-col'>
+        <input className="p-2 m-2 mt-10 border"
+          type="text"
+          placeholder="Task Name"
+          value={recipeName}
+          onChange={e => setRecipeName(e.target.value)} />
+        <input className="p-2 m-2 border"
+          type="text"
+          placeholder="Task Description"
+          value={recipeDesc}
+          onChange={e => setRecipeDesc(e.target.value)} />
+        <button className="bg-green-500 hover:bg-green-700 p-2 m-2 rounded-lg text-white"
+          onClick={addTaskClick}>
+          Add Task
+        </button>
+      </div>
+    </div>
   )
 }
 
