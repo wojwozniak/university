59269c4b752b5370fb76c61d9db47a53e1285d3b
Wojtek <pulchnymamut@gmail.com>
2024-03-15 20:56:34 +0100

    use filters

--- React/List04/src/components/RecipeList.tsx
+++ React/List04/src/components/RecipeList.tsx
@@ -1,15 +1,41 @@
 import { useContext } from 'react'
 import { RecipeContext } from '../RecipeContext';
 import SingleRecipe from './SingleRecipe';
+import { Recipe } from '../interfaces/Recipe';
 
 const RecipeList = () => {
   const { state } = useContext(RecipeContext);
+  const { filterForFavorite, searchQuery, recipes, favorites } = state;
+
+  const getFavoriteRecipes = (recipes: Recipe[]): Recipe[] => {
+    return recipes.filter(recipe => favorites.includes(recipe.id));
+  }
+
+  const getFilteredRecipes = (recipes: Recipe[]): Recipe[] => {
+    if (searchQuery === '') return recipes;
+    return recipes.filter(recipe => recipe.name.toLowerCase().includes(searchQuery.toLowerCase()));
+
+  }
+
+  const getRecipesAfterFilter = (recipes: Recipe[]): Recipe[] => {
+    if (filterForFavorite && searchQuery.length > 0) {
+      return getFavoriteRecipes(getFilteredRecipes(recipes));
+    }
+    else if (filterForFavorite) {
+      return getFavoriteRecipes(recipes);
+    } else if (searchQuery.length > 0) {
+      return getFilteredRecipes(recipes);
+    } else {
+      return recipes;
+    }
+  }
 
   return (
     <main id="list" className='w-3/5 p-2'>
-      {state.recipes.map((recipe, index) => <SingleRecipe key={index} recipe={recipe} />)}
+      {getRecipesAfterFilter(recipes).map((recipe, index) => <SingleRecipe key={index} recipe={recipe} />)}
     </main>
   )
 }
+
 
 export default RecipeList
