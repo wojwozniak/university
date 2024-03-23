6bc37f6ec8587bf65b9ec725474a8fb42f9e9801
Wojtek <pulchnymamut@gmail.com>
2024-03-23 20:20:53 +0100

    task1 rdy

---
+++ POO/List0004/SingletonTests.cs
@@ -0,0 +1,34 @@
+namespace List0004
+{
+    public class SingletonTests
+    {
+        [Fact]
+        public void TestInstanceProcessWide()
+        {
+            var instance1 = Singleton.InstanceProcessWide;
+            var instance2 = Singleton.InstanceProcessWide;
+            Assert.Same(instance1, instance2);
+        }
+
+        [Fact]
+        public void TestInstancePerThread()
+        {
+            var instance1 = Singleton.InstancePerThread;
+            var instance2 = Singleton.InstancePerThread;
+            Assert.Same(instance1, instance2);
+        }
+
+        [Fact]
+        public void TestInstanceForFiveSeconds()
+        {
+            var instance1 = Singleton.InstanceForFiveSeconds;
+            Thread.Sleep(2000); // sleep 2s
+            var instance2 = Singleton.InstanceForFiveSeconds;
+            Assert.Same(instance1, instance2);
+
+            Thread.Sleep(5000); // sleep 5s
+            var instance3 = Singleton.InstanceForFiveSeconds;
+            Assert.NotSame(instance2, instance3);
+        }
+    }
+}
---
+++ POO/List0004/task1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List0004
+{
+    public class Singleton
+    {
+        private static readonly Lazy<Singleton> instanceProcessWide =
+            new Lazy<Singleton>(() => new Singleton(), LazyThreadSafetyMode.ExecutionAndPublication);
+
+        private static readonly ThreadLocal<Singleton> instancePerThread =
+            new ThreadLocal<Singleton>(() => new Singleton());
+
+        private static Singleton? instanceForFiveSeconds;
+        private static readonly object lockObject = new object();
+        private static DateTime lastAccessTime;
+
+        public static Singleton? InstanceProcessWide => instanceProcessWide.Value;
+
+        public static Singleton? InstancePerThread => instancePerThread.Value;
+
+        public static Singleton InstanceForFiveSeconds
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (instanceForFiveSeconds == null || (DateTime.Now - lastAccessTime).TotalSeconds >= 5)
+                    {
+                        instanceForFiveSeconds = new Singleton();
+                        lastAccessTime = DateTime.Now;
+                    }
+                    return instanceForFiveSeconds;
+                }
+            }
+        }
+
+        private Singleton() { }
+    }
+}
