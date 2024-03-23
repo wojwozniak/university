aaef88ac8343fcabf5ca701d2ffda7df95c2d4cc
Wojtek <pulchnymamut@gmail.com>
2024-03-24 00:50:25 +0100

    about reworked

--- React/List05/scss/src/Sections.tsx
+++ React/List05/scss/src/Sections.tsx
@@ -1,6 +1,5 @@
 import Services from "./sections/Services";
 import Contact from "./sections/Contact";
-import About from "./sections/About";
 import Team from "./sections/Team";
 import Blog from "./sections/Blog";
 
@@ -9,6 +8,7 @@ import "./Sections.scss"
 import { Service } from "./types/Service";
 import { TeamMembers } from "./types/TeamMembers";
 import { BlogPost } from "./types/BlogPost";
+import Section from "./sections/ui/Service";
 
 interface SectionsProps {
   about: string;
@@ -20,7 +20,9 @@ interface SectionsProps {
 const Sections: React.FC<SectionsProps> = ({ about, services, teamMembers, blogPosts }) => {
   return (
     <div className="content-card">
-      <About about={about} />
+      <Section name="about" headerText="About">
+        <>{about}</>
+      </Section>
       <Services services={services} />
       <Team teamMembers={teamMembers} />
       <Blog blogPosts={blogPosts} />
