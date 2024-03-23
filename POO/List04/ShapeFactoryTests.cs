23e3dc8b7bd466dc22415d4966d1ff5db3ddcc11
Wojtek <pulchnymamut@gmail.com>
2024-03-23 20:45:44 +0100

    task2 done

---
+++ POO/List04/ShapeFactoryTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List04
+{
+    public class ShapeFactoryTests
+    {
+        public class Square : IShape
+        {
+            private readonly double sideLength;
+
+            public Square(double sideLength)
+            {
+                this.sideLength = sideLength;
+            }
+
+            public double CalculateArea()
+            {
+                return sideLength * sideLength;
+            }
+        }
+
+        public class Rectangle : IShape
+        {
+            private readonly double length;
+            private readonly double width;
+
+            public Rectangle(double length, double width)
+            {
+                this.length = length;
+                this.width = width;
+            }
+
+            public double CalculateArea()
+            {
+                return length * width;
+            }
+        }
+
+        public class SquareFactoryWorker : IShapeFactoryWorker
+        {
+            public IShape CreateShape(params object[] parameters)
+            {
+                if (parameters.Length != 1 || (parameters[0] is not double && parameters[0] is not int))
+                {
+                    throw new ArgumentException("Invalid parameters");
+                }
+
+                double sideLength = Convert.ToDouble(parameters[0]);
+                return new Square(sideLength);
+            }
+        }
+
+        public class RectangleFactoryWorker : IShapeFactoryWorker
+        {
+            public IShape CreateShape(params object[] parameters)
+            {
+                if (parameters.Length != 2 || (parameters[0] is not double && parameters[0] is not int) || (parameters[1] is not double && parameters[0] is not int))
+                {
+                    throw new ArgumentException("Invalid parameters");
+                }
+
+                double length = Convert.ToDouble(parameters[0]);
+                double width = Convert.ToDouble(parameters[1]);
+                return new Rectangle(length, width);
+            }
+        }
+
+        [Fact]
+        public void CreateSquareTest()
+        {
+            var factory = new ShapeFactory();
+            var squareWorker = new SquareFactoryWorker();
+            factory.RegisterWorker(squareWorker);
+
+            var square = factory.CreateShape("Square", 5);
+
+            Assert.Equal(25, square.CalculateArea());
+        }
+
+        [Fact]
+        public void CreateRectangleTest()
+        {
+            var factory = new ShapeFactory();
+            var rectangleWorker = new RectangleFactoryWorker();
+            factory.RegisterWorker(rectangleWorker);
+
+            var rectangle = factory.CreateShape("Rectangle", 3, 5);
+
+            Assert.Equal(15, rectangle.CalculateArea());
+        }
+    }
+}
---
+++ POO/List04/task2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List04
+{
+    public interface IShapeFactoryWorker
+    {
+        IShape CreateShape(params object[] parameters);
+    }
+
+    public interface IShape
+    {
+        double CalculateArea();
+    }
+
+    public class ShapeFactory
+    {
+        private IShapeFactoryWorker? worker;
+
+        public void RegisterWorker(IShapeFactoryWorker worker)
+        {
+            this.worker = worker;
+        }
+
+        public IShape CreateShape(string shapeName, params object[] parameters)
+        {
+            if (worker == null)
+            {
+                throw new InvalidOperationException("No registration");
+            }
+
+            return worker.CreateShape(parameters);
+        }
+    }
+}
