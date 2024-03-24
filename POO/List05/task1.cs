beffce17e89e8aa9bc6b0d647d408428a7913d1e
Wojtek <pulchnymamut@gmail.com>
2024-03-24 18:08:35 +0100

    Update task1.cs

--- POO/List05/task1.cs
+++ POO/List05/task1.cs
@@ -1,37 +1,25 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Net;
 using System.Net.Mail;
 
+// Fasada - Uproszczony 
 class SmtpFacade
 {
-    public static void Send(string from, string to, string subject, string body, Stream attachment, string attachmentMimeType)
+    public static void Send(string From, string To, string Subject, string Body, Stream? Attachment, string? AttachmentMimeType)
     {
-        try
+        using MailMessage mailMessage = new();
+        mailMessage.From = new MailAddress(From);
+        mailMessage.To.Add(To);
+        mailMessage.Subject = Subject;
+        mailMessage.Body = Body;
+
+        if (Attachment is not null)
         {
-            using SmtpClient client = new("your_smtp_server_address");
-            client.Port = 587; // Adjust the port number if necessary
-            client.Credentials = new NetworkCredential("your_smtp_username", "your_smtp_password");
-            client.EnableSsl = true;
-
-            using MailMessage mailMessage = new();
-
-            mailMessage.From = new MailAddress(from);
-            mailMessage.To.Add(to);
-            mailMessage.Subject = subject;
-            mailMessage.Body = body;
-
-            if (attachment != null)
-            {
-                Attachment mailAttachment = new(attachment, attachmentMimeType);
-                mailMessage.Attachments.Add(mailAttachment);
-            }
-
-            client.Send(mailMessage);
+            Attachment attachment = new(Attachment, AttachmentMimeType);
+            mailMessage.Attachments.Add(attachment);
         }
-        catch (Exception ex)
-        {
-            Console.WriteLine("Error occurred while sending email: " + ex.Message);
-        }
+
+        using SmtpClient smtpClient = new("server_adress");
+        smtpClient.Send(mailMessage);
     }
 }
