a9ac4e92ad2b0dc6f630e9d77ffb574f50da3c0f
Wojtek <pulchnymamut@gmail.com>
2024-03-24 01:50:26 +0100

    copy changes from previous version

--- React/List05/emotion/src/components/sections/About.tsx
+++ React/List05/emotion/src/components/sections/About.tsx
@@ -2,7 +2,7 @@ import Section from '../../ui/Section'
 
 const About = ({ about }: { about: string }) => {
   return (
-    <Section name="about" headerText="About">
+    <Section name="about" headerText="About Us">
       <p>{about}</p>
     </Section>
   )
