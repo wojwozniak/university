9cbf005d7dc1f8d87535cf1d81038f5cb213f4e7
Wojtek <pulchnymamut@gmail.com>
2024-03-24 17:57:23 +0100

    temp

--- React/List05/emotion/src/ui/TeamCard.tsx
+++ React/List05/emotion/src/ui/TeamCard.tsx
@@ -1,9 +1,43 @@
+import { useTheme } from '@emotion/react'
 import { TeamMembers } from '../types/TeamMembers'
 import './TeamCard.scss'
 
 const TeamCard = ({ member }: { member: TeamMembers }) => {
+
+  /*
+  
+.team-member {
+  flex: 0 0 calc(33.33% - 20px);
+  padding: 20px;
+  margin: 10px;
+  text-align: center;
+}
+
+.team-member img {
+  border-radius: 50%;
+  margin-bottom: 20px;
+}
+
+.team-member h3 {
+  margin-bottom: 10px;
+  display: inline-block;
+}
+
+.light-theme .team-member {
+  background-color: variables.$tainted-white;
+  color: variables.$gray;
+}
+
+.dark-theme .team-member {
+  background-color: variables.$light-gray;
+  color: variables.$white;
+}
+*/
+
+  const theme = useTheme();
+
   return (
-    <div key={member.id} className="team-member">
+    <div css={{ color: theme.color }} key={member.id} className="team-member">
       <img src={member.image} alt={member.name} />
       <div>
         <h3>{member.name}</h3>
