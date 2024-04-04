abce820d7c80ff86cf69ccc6e85d506b33e81b5c
Wojtek <pulchnymamut@gmail.com>
2024-04-04 21:21:20 +0200

    footer

--- React/List06/src/components/Footer.tsx
+++ React/List06/src/components/Footer.tsx
@@ -1,6 +1,9 @@
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className="text-lg flex flex-col items-center mt-2">
+      <hr className="border border-gray-400 w-[90%]" />
+      <p className="mt-1">Copyright 2024 | Cool Shop</p>
+    </footer>
   )
 }
 
