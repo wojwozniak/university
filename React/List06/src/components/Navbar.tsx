f734cb1924d8c20e9bf7e23880d24b8169c20116
Wojtek <pulchnymamut@gmail.com>
2024-04-05 16:41:04 +0200

    Update Navbar.tsx

--- React/List06/src/components/Navbar.tsx
+++ React/List06/src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import User from "./User"
 
 const Navbar = () => {
   return (
-    <nav className="pl-10 pr-10 flex flex-row justify-between items-center w-full mt-2">
+    <nav className="pl-10 pr-10 flex flex-row justify-between items-center w-full p-2 bg-gray-500">
       <Logo />
       <User />
     </nav>
