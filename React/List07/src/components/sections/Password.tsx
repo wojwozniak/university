ea38904ad5ad3b597dd337757534fae658ba2b9c
Wojtek <pulchnymamut@gmail.com>
2024-04-12 18:05:13 +0200

    create content

--- React/List07/src/components/sections/Password.tsx
+++ React/List07/src/components/sections/Password.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '../../ui/Button'
+import Input from '../../ui/Input'
 
 const Password = () => {
+  const [password, setPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleNewPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(event.target.value);
+  };
+
   return (
     <div id="password">
-      <h2>
+      <h2 className='text-l mb-5'>
         Change your password here. After saving, you'll be logged out.
       </h2>
+      <Input label={"Current password:"} value={password} handleValueChange={handlePasswordChange} />
+      <Input label={"New password:"} value={newPassword} handleValueChange={handleNewPasswordChange} />
+      <Input label={"Confirm password:"} value={confirmPassword} handleValueChange={handleConfirmPasswordChange} />
       <Button text='Change password' onClick={() => { }} />
     </div>
   )
