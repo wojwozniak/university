eadd4fd922bfbf4d814677e81a015424b45dd47d
Wojtek <pulchnymamut@gmail.com>
2024-04-12 17:57:13 +0200

    Update Input.tsx

--- React/List07/src/ui/Input.tsx
+++ React/List07/src/ui/Input.tsx
@@ -10,13 +10,13 @@ interface InputProps {
 const Input: React.FC<InputProps> = ({ value, handleValueChange }) => {
   return (
     <div className='flex flex-col'>
-      <Label.Root htmlFor="name" className="Label">Full Name:</Label.Root>
+      <Label.Root htmlFor="name">Full Name:</Label.Root>
       <input
         type="text"
         id="name"
         value={value}
         onChange={handleValueChange}
-        className="border border-gray-300 rounded px-3 py-2 w-4/5 bg-white"
+        className="border border-gray-300 rounded px-3 py-2 w-4/5 bg-white text-purple-500"
       />
     </div>
   )
