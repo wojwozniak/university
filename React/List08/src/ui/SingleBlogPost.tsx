45d50c35a5671b8c4ea28f661267a695d125e722
Wojtek <pulchnymamut@gmail.com>
2024-04-21 01:38:49 +0200

    SingleBlogPost ready

--- React/List08/src/ui/SingleBlogPost.tsx
+++ React/List08/src/ui/SingleBlogPost.tsx
@@ -1,13 +1,15 @@
 import { BlogPost } from '../types/BlogPost'
-import './SingleBlogPost.scss'
 
 const SingleBlogPost = ({ post }: { post: BlogPost }) => {
   return (
-    <div key={post.id} className="blog-post">
-      <h3>{post.title}</h3>
-      <p>{post.date}</p>
-      <p>{post.content}</p>
-      <button>Read More</button>
+    <div key={post.id} className="rounded-lg p-5 text-left dark:bg-black-tainted bg-tained-harder-white text-gray">
+      <h3 className='mb-[10px]'>{post.title}</h3>
+      <p className='mb-[10px]'>{post.date}</p>
+      <p className='mb-[10px]'>{post.content}</p>
+      <button
+        className="border-none rounded-md cursor-pointer py-2 px-4 transition duration-300 ease-in-out hover:bg-green dark:bg-light-green dark:text-white dark:hover:bg-green text-white bg-light-green color-white hover:bg-green">
+        Read More
+      </button>
     </div>
   )
 }
