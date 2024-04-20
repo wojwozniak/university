c8c3399f88837decd6bf3780ae4363ec782f712f
Wojtek <pulchnymamut@gmail.com>
2024-04-21 01:08:45 +0200

    add tailwind, define themes

--- React/List08/src/App.tsx
+++ React/List08/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.scss";
+import "./index.css";
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
---
+++ React/List08/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        white: '#fff',
+        'white-darker': '#eee',
+        'tainted-white': '#f5f5f5',
+        'tained-harder-white': '#f0f0f0',
+        dark: '#111',
+        'black-tainted': '#222',
+        gray: '#333',
+        'light-gray': '#444',
+        'very-light-gray': '#666',
+        green: '#45a049',
+        'light-green': '#4caf50',
+      },
+    }
+  },
+  plugins: [],
+}
+
