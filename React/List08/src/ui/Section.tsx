812164c2ca6204b985a2e1cebca97cc0328a23a8
Wojtek <pulchnymamut@gmail.com>
2024-04-21 01:26:36 +0200

    section

--- React/List08/src/ui/Section.tsx
+++ React/List08/src/ui/Section.tsx
@@ -9,9 +9,9 @@ interface SectionProps {
 
 const Section: React.FC<SectionProps> = ({ children, headerText, name }) => {
   return (
-    <section id={name} className={"section " + name}>
-      <div className="section-content">
-        <h2>
+    <section id={name} className={"py-5 " + name}>
+      <div className="max-w-800 mx-auto">
+        <h2 className='text-3xl mb-5 inline-block'>
           {headerText}
         </h2>
         {children}
