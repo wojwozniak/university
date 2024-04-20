b61246cc32dc7880aea1e8e421a05b47a6409436
Wojtek <pulchnymamut@gmail.com>
2024-04-21 01:44:10 +0200

    teamcard reworked

--- React/List08/src/ui/TeamCard.tsx
+++ React/List08/src/ui/TeamCard.tsx
@@ -1,12 +1,11 @@
 import { TeamMembers } from '../types/TeamMembers'
-import './TeamCard.scss'
 
 const TeamCard = ({ member }: { member: TeamMembers }) => {
   return (
-    <div key={member.id} className="team-member">
-      <img src={member.image} alt={member.name} />
+    <div key={member.id} className="w-1/3 p-5 m-[10px] text-center bg-tained-white text-gray dark:bg-light-gray dark:text-white"> {/* #TODO 1/3 - 20px */}
+      <img src={member.image} alt={member.name} className='rounded-full mb-5' />
       <div>
-        <h3>{member.name}</h3>
+        <h3 className='mb-4 inline-block'>{member.name}</h3>
         <p>{member.position}</p>
         <p>{member.bio}</p>
       </div>
