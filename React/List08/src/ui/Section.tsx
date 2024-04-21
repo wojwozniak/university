a16546671df504c0e4e6f010350ac20e3ff4a1ff
Wojtek <pulchnymamut@gmail.com>
2024-04-21 02:27:28 +0200

    section

--- React/List08/src/ui/Section.tsx
+++ React/List08/src/ui/Section.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import "./Section.scss";
 
 interface SectionProps {
   children: React.ReactNode;
@@ -9,7 +8,9 @@ interface SectionProps {
 
 const Section: React.FC<SectionProps> = ({ children, headerText, name }) => {
   return (
-    <section id={name} className={"py-5 " + name}>
+    <section id={name}
+      className={"py-5 [&:nth-child(even)]:bg-tainted-white " + name}>
+      {/* #TODO [&:nth-child(even)]:dark:bg-light-gray */}
       <div className="max-w-800 mx-auto">
         <h2 className='text-3xl mb-5 inline-block'>
           {headerText}
