ed4f9add05491e9b55e6223028cbade1291eb70a
Wojtek <pulchnymamut@gmail.com>
2024-04-21 02:21:49 +0200

    contactform ready

--- React/List08/src/ui/ContactForm.tsx
+++ React/List08/src/ui/ContactForm.tsx
@@ -7,19 +7,33 @@ const ContactForm = () => {
   };
 
   const elems = [
-    <input type="text" placeholder="Name" required />,
-    <input type="email" placeholder="Email" required />,
-    <textarea rows={5} placeholder="Message" required></textarea>,
+    <input type="text"
+      placeholder="Name"
+      required
+      className='w-full p-2 rounded-md border-none mt-1 bg-white text-gray border dark:bg-light-gray dark:text-white' />,
+    <input type="email"
+      placeholder="Email"
+      required
+      className='w-full p-2 rounded-md border-none mt-1 bg-white text-gray border dark:bg-light-gray dark:text-white' />,
+    <textarea rows={5}
+      placeholder="Message"
+      required
+      className='w-full p-2 rounded-md border-none mt-1 resize-y bg-white text-gray border dark:bg-light-gray dark:text-white' />,
   ]
 
   return (
-    <form onSubmit={handleSubmit} className="mb-10 contact-form">
+    <form onSubmit={handleSubmit}
+      className="mb-10 max-w-[500px] mx-auto p-5 rounded-lg flex flex-col bg-white text-gray border dark:bg-gray dark:text-white">
       {elems.map((elem, index) => (
-        <div key={index} className="form-group">
+        <div key={index}
+          className="mb-5">
           {elem}
         </div>
       ))}
-      <button type="submit">Send Message</button>
+      <button type="submit"
+        className='py-[10px] px-5 b-0 rounded cursor-pointer transition duration-300 ease-in-out bg-light-green text-white hover:bg-green dark:bg-light-green dark:text-white dark:hover:bg-green'>
+        Send Message
+      </button>
     </form>
   )
 }
