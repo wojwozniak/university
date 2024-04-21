96b596f827e95dd745a6e106b85cd0cb7ad725cb
Wojtek <pulchnymamut@gmail.com>
2024-04-21 02:06:52 +0200

    blog

--- React/List08/src/components/sections/Blog.tsx
+++ React/List08/src/components/sections/Blog.tsx
@@ -1,12 +1,11 @@
 import Section from '../../ui/Section'
 import SingleBlogPost from '../../ui/SingleBlogPost'
 import { BlogPost } from '../../types/BlogPost'
-import './Blog.scss'
 
 const Blog = ({ blogPosts }: { blogPosts: BlogPost[] }) => {
   return (
     <Section name="blog" headerText="Latest Blog Posts">
-      <div className="blog-posts">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
         {blogPosts.map((post) => <SingleBlogPost post={post} key={post.id} />)}
       </div>
     </Section>
