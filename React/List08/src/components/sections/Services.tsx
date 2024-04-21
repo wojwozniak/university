f80621a9ce60a55c51ceb149634c6ad3cd55e47f
Wojtek <pulchnymamut@gmail.com>
2024-04-21 02:05:32 +0200

    services

--- React/List08/src/components/sections/Services.tsx
+++ React/List08/src/components/sections/Services.tsx
@@ -1,12 +1,11 @@
 import { Service } from '../../types/Service'
 import Section from '../../ui/Section'
 import SingleService from '../../ui/SingleService'
-import './Services.scss'
 
 const Services = ({ services }: { services: Service[] }) => {
   return (
     <Section name="services" headerText="Services">
-      <ul>
+      <ul className='list-none p-0 m-0'>
         {services.map((service) => <SingleService service={service} key={service.id} />)}
       </ul>
     </Section>
--- React/List08/src/ui/SingleService.tsx
+++ React/List08/src/ui/SingleService.tsx
@@ -2,8 +2,8 @@ import { Service } from '../types/Service'
 
 const SingleService = ({ service }: { service: Service }) => {
   return (
-    <li key={service.id}>
-      <h3>{service.name}</h3>
+    <li key={service.id} className='mb-5'>
+      <h3 className='text-2xl mb-[10px]'>{service.name}</h3>
       <p>{service.description}</p>
     </li>
   )
