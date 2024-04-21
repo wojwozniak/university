61876a2d2427e46f041c0041060ade67d4cb971c
Wojtek <pulchnymamut@gmail.com>
2024-04-21 02:33:56 +0200

    style fixes

--- React/List08/src/ui/TeamCard.tsx
+++ React/List08/src/ui/TeamCard.tsx
@@ -2,7 +2,9 @@ import { TeamMembers } from '../types/TeamMembers'
 
 const TeamCard = ({ member }: { member: TeamMembers }) => {
   return (
-    <div key={member.id} className="w-1/3 p-5 m-[10px] flex flex-col text-center bg-tainted-white text-gray dark:bg-light-gray dark:text-white"> {/* #TODO 1/3 - 20px */}
+    <div key={member.id}
+      className="w-1/3 p-5 m-[10px] flex flex-col text-center bg-tainted-white text-gray dark:bg-light-gray dark:text-white">
+      {/* #TODO 1/3 - 20px */}
       <img src={member.image} alt={member.name} className='rounded-full mb-5' />
       <div>
         <h3 className='mb-4 inline-block'>{member.name}</h3>
