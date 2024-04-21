cb9a965b05039ee44798159d7d4df473c1fbe7a9
Wojtek <pulchnymamut@gmail.com>
2024-04-21 20:55:36 +0200

    Update task6.ml

--- Metody Programowania/List03/task6.ml
+++ Metody Programowania/List03/task6.ml
@@ -24,3 +24,23 @@ let rec fold_tree f a t =
   match t with
   | Leaf -> a
   | Node (l, v, r) -> f (fold_tree f a l) v (fold_tree f a r)
+
+
+let tree_product t =
+  fold_tree (fun l v r -> l * v * r) 1 t
+
+let tree_flip t =
+  fold_tree (fun l v r -> Node (r, v, l)) Leaf t
+
+let tree_height t =
+  fold_tree (fun l _ r -> 1 + max l r) 0 t
+
+let tree_span t =
+  fold_tree (fun l v r -> (min l v, max v r)) (max_int, min_int) t
+  
+let preorder t =
+  let rec preorder' t acc =
+    match t with
+    | Leaf -> acc
+    | Node (l, v, r) -> v :: (preorder' l (preorder' r acc))
+  in preorder' t []
