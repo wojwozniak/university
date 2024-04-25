b0ea1df26a70c931abd9ec051a9e726daeb16465
Wojtek <pulchnymamut@gmail.com>
2024-04-26 01:54:41 +0200

    fixes

--- React/List08/src/ui/Section.tsx
+++ React/List08/src/ui/Section.tsx
@@ -9,8 +9,7 @@ interface SectionProps {
 const Section: React.FC<SectionProps> = ({ children, headerText, name }) => {
   return (
     <section id={name}
-      className={"py-5 [&:nth-child(even)]:bg-tainted-white " + name}>
-      {/* #TODO [&:nth-child(even)]:dark:bg-light-gray */}
+      className={"py-5 even:bg-tainted-white dark:even:bg-light-gray " + name}>
       <div className="max-w-800 mx-auto">
         <h2 className='text-4xl font-bold mb-5 inline-block'>
           {headerText}
--- React/List08/src/ui/TeamCard.tsx
+++ React/List08/src/ui/TeamCard.tsx
@@ -3,8 +3,7 @@ import { TeamMembers } from '../types/TeamMembers'
 const TeamCard = ({ member }: { member: TeamMembers }) => {
   return (
     <div key={member.id}
-      className="w-1/3 p-5 m-[10px] flex flex-col text-center bg-tainted-white text-gray dark:bg-light-gray dark:text-white">
-      {/* #TODO 1/3 - 20px  /// w-calc */}
+      className="w-[calc(33.33%-20px)] p-5 m-[10px] flex flex-col text-center bg-tainted-white text-gray dark:bg-light-gray dark:text-white">
       <img src={member.image} alt={member.name} className='rounded-full mb-5' />
       <div>
         <h3 className='mb-4 inline-block'>{member.name}</h3>
