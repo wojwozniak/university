2b34b3a08ab4cf67c056d01c4d055868878feca5
Wojtek <pulchnymamut@gmail.com>
2024-04-26 03:40:28 +0200

    symulacja ładowania

--- React/List09/src/ui/Pfp.tsx
+++ React/List09/src/ui/Pfp.tsx
@@ -1,15 +1,33 @@
-import pfp from "../assets/pfp.png";
+import { useEffect, useState } from 'react';
+import { Skeleton } from '@mui/material';
+import pfp from '../assets/pfp.png';
 
 const Pfp = () => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  const handleImageLoaded = () => {
+    setImageLoaded(true);
+  };
+  useEffect(() => {
+    handleImageLoaded();
+    return () => {
+      setImageLoaded(false);
+    };
+  }, []);
+
   return (
     <div className="rounded-full overflow-hidden border-4 border-white h-48 w-48 flex items-center justify-center">
+      {!imageLoaded && (
+        <Skeleton animation="wave" width={192} height={192} variant="circular" />
+      )}
       <img
-        className="h-full w-full object-cover"
+        className={`h-full w-full object-cover ${!imageLoaded ? 'hidden' : ''}`}
         src={pfp}
         alt="Zdjęcie profilowe"
+        onLoad={handleImageLoaded}
       />
     </div>
-  )
-}
+  );
+};
 
-export default Pfp
+export default Pfp;
