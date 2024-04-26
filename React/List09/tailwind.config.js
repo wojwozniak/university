f3f9ed133ee8321ac7f7b7a03f1b040230a1e307
Wojtek <pulchnymamut@gmail.com>
2024-04-26 03:19:26 +0200

    define bg color

--- React/List09/tailwind.config.js
+++ React/List09/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'background': '#f2f2f2',
+      },
+    },
   },
   plugins: [],
 }
