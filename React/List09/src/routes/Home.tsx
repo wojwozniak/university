b21e5434f1de537421043bc65be492ac55c15df4
Wojtek <pulchnymamut@gmail.com>
2024-04-26 03:34:09 +0200

    base homepage design finished

--- React/List09/src/routes/Home.tsx
+++ React/List09/src/routes/Home.tsx
@@ -1,7 +1,21 @@
+import Pfp from "../ui/Pfp";
+
+
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="container mx-auto px-4 py-8 flex flex-col items-center">
+      <Pfp />
+      <h1 className="text-3xl font-bold mt-8">
+        Wojciech Woźniak
+      </h1>
+      <p className="text-lg text-center mt-4">
+        Cześć! Jestem front-end developerem.
+      </p>
+      <p className="text-lg text-center mt-2">
+        Specjalizuję się w tworzeniu niezwykłych stron internetowych.
+      </p>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
