1846a799ea520146d10928c64ceda25b9d0199ca
Wojtek <pulchnymamut@gmail.com>
2024-04-26 04:12:10 +0200

    base files from mui docs

---
+++ React/List09/src/routes/About/Infotabs.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Box, Tab, Tabs } from "@mui/material"
+import { useState } from "react";
+import CustomTabPanel from "../../ui/CustomTabPanel";
+
+const Infotabs = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (_event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="Item One" />
+          <Tab label="Item Two" />
+          <Tab label="Item Three" />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        Item One
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        Item Two
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        Item Three
+      </CustomTabPanel>
+    </div>
+  )
+}
+
+export default Infotabs
---
+++ React/List09/src/ui/CustomTabPanel.tsx
@@ -0,0 +1,25 @@
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <>{children}</>
+      )}
+    </div>
+  );
+}
+
+export default CustomTabPanel;
