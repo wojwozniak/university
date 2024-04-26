d70260905956eb6dd994f17ce6d6d13b56dd0ab2
Wojtek <pulchnymamut@gmail.com>
2024-04-26 03:56:23 +0200

    sizing

--- React/List09/src/ui/Pfp.tsx
+++ React/List09/src/ui/Pfp.tsx
@@ -2,7 +2,11 @@ import { useEffect, useState } from 'react';
 import { Skeleton } from '@mui/material';
 import pfp from '../assets/pfp.png';
 
-const Pfp = () => {
+interface PfpProps {
+  size: string;
+}
+
+const Pfp: React.FC<PfpProps> = ({ size }) => {
   const [imageLoaded, setImageLoaded] = useState(false);
 
   const handleImageLoaded = () => {
@@ -15,10 +19,27 @@ const Pfp = () => {
     };
   }, []);
 
+  const bigSize = {
+    widthString: '192px',
+    heightString: '192px',
+    width: 192,
+    height: 192
+  };
+
+  const smallSize = {
+    widthString: '48px',
+    heightString: '48px',
+    width: 48,
+    height: 48
+  };
+
+  const setting = size === 'big' ? bigSize : smallSize;
+
   return (
-    <div className="rounded-full overflow-hidden border-4 border-white h-48 w-48 flex items-center justify-center">
+    <div className="rounded-full overflow-hidden border-4 border-white flex items-center justify-center"
+      style={{ width: setting.widthString, height: setting.heightString }}>
       {!imageLoaded && (
-        <Skeleton animation="wave" width={192} height={192} variant="circular" />
+        <Skeleton animation="wave" width={setting.width} height={setting.height} variant="circular" />
       )}
       <img
         className={`h-full w-full object-cover ${!imageLoaded ? 'hidden' : ''}`}
