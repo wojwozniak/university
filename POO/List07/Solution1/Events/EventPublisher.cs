d6fb6f08ccdba312887d47185771ea2998df9294
Wojtek <pulchnymamut@gmail.com>
2024-05-01 18:44:30 +0200

    eventpublisher

---
+++ POO/List07/Solution1/Events/EventPublisher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Solution1.Events
+{
+    public class EventPublisher : IEventPublisher
+    {
+        private readonly ConcurrentDictionary<Type, object> _subjects
+            = new();
+
+        public IObservable<TEvent> GetEvent<TEvent>()
+        {
+            var subject =
+                (ISubject<TEvent>)_subjects.GetOrAdd(typeof(TEvent),
+                                                      t => new Subject<TEvent>());
+            return subject.AsObservable();
+        }
+
+        public void Publish<TEvent>(TEvent sampleEvent)
+        {
+            object subject;
+            if (_subjects.TryGetValue(typeof(TEvent), out subject))
+            {
+                ((ISubject<TEvent>)subject)
+                    .OnNext(sampleEvent);
+            }
+        }
+    }
+}
---
+++ POO/List07/Solution1/Events/IEventPublisher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution1.Events
+{
+    public interface IEventPublisher
+    {
+        void Publish<TEvent>(TEvent sampleEvent);
+        IObservable<TEvent> GetEvent<TEvent>();
+    }
+}
