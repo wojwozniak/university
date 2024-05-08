dd667330c89abaae64f505e6654aa6621d1172de
Wojtek <pulchnymamut@gmail.com>
2024-05-08 02:29:48 +0200

    Create task1-l4.sql

---
+++ Bazy Danych/List08/task1-l4.sql
@@ -0,0 +1,25 @@
+WITH 
+  offer_pairs AS (
+    SELECT DISTINCT company_branch_id, company_id FROM offer
+  ),
+  true_pairs AS (
+    SELECT DISTINCT id AS company_branch_id, company_id FROM company_branch
+  ),
+  outputed AS (
+	SELECT 
+		true_pairs.company_branch_id, 
+		offer_pairs.company_id,
+		true_pairs.company_id as "target_value"
+			FROM true_pairs 
+				JOIN offer_pairs ON true_pairs.company_branch_id = offer_pairs.company_branch_id
+					where offer_pairs.company_id <> true_pairs.company_id
+	  )
+update offer
+	set company_id = (select target_value from outputed)
+	where 
+		EXISTS (
+    		SELECT 1 
+			   FROM outputed WHERE 
+					  offer.company_branch_id = outputed.company_branch_id
+					  AND offer.company_id = outputed.company_id
+				  );
