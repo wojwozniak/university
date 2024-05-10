0f0ac01bf04bc52d70e3e4bb06bb666a587dd21d
Wojtek <pulchnymamut@gmail.com>
2024-05-11 01:23:51 +0200

    use contextProvider

--- React/List10/src/App.tsx
+++ React/List10/src/App.tsx
@@ -1,8 +1,9 @@
+import { InfoProvider } from "./InfoContext"
 import Main from "./components/Main"
 
 function App() {
   return (
-    <div id="root">
+    <InfoProvider>
       <div className="bg-gradient-to-b from-purple-900 via-purple-500 to-blue-300 h-screen flex justify-center items-center">
         <div className="bg-blue-300 w-96 h-96 rounded-xl flex flex-col justify-between items-center">
           <div className="h-1/6 bg-blue-900 w-full rounded-t-xl p-4 text-white text-2xl font-bold">
@@ -12,7 +13,7 @@ function App() {
           <div className="h-1/6 bg-blue-900 w-full rounded-b-xl"></div>
         </div>
       </div>
-    </div>
+    </InfoProvider>
   )
 }
 
