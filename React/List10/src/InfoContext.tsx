91f81b69c29e2ec3ba1a4946b420a4b80da321d9
Wojtek <pulchnymamut@gmail.com>
2024-05-11 01:23:44 +0200

    contextProvider

---
+++ React/List10/src/InfoContext.tsx
@@ -0,0 +1,48 @@
+import { createContext, useReducer } from "react";
+import { State } from "./interfaces/State";
+import { Action } from "./interfaces/Action";
+
+const initialState: State = {
+  user: "",
+  currentScore: 0,
+  inGame: false,
+  highScore: 0,
+  data: []
+}
+
+const stateReducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'ADD_POINTS':
+      state.currentScore += 1;
+      if (state.currentScore > state.highScore) {
+        state.highScore = state.currentScore;
+      }
+      return state;
+    case 'ZERO_POINTS':
+      state.currentScore = 0;
+      return state;
+    case 'CATEGORY_SCREEN':
+      state.inGame = false;
+      return state;
+    default:
+      return state;
+  };
+}
+
+export const InfoContext = createContext<{
+  state: State;
+  dispatch: React.Dispatch<Action>;
+}>({
+  state: initialState,
+  dispatch: () => null,
+});
+
+export const InfoProvider = ({ children }: { children: React.ReactNode }) => {
+  const [state, dispatch] = useReducer(stateReducer, initialState);
+
+  return (
+    <InfoContext.Provider value={{ state, dispatch }}>
+      {children}
+    </InfoContext.Provider>
+  );
+};
