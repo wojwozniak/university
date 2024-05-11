052d8007a5e9c39c39ffcdce3a48fb4c4675991b
Wojtek <pulchnymamut@gmail.com>
2024-05-11 02:06:49 +0200

    store endpoints

---
+++ React/List10/src/endpoints.ts
@@ -0,0 +1,6 @@
+import { Endpoint } from "./interfaces/Endpoint";
+
+export const endpoints: Endpoint[] = [
+  { category: "Potions", url: "https://api.potterdb.com/v1/potions" },
+  { category: "Spells", url: "https://api.potterdb.com/v1/spells" }
+];
