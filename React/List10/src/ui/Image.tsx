212d79c66aacd722d372bf7834700bdf60ecfee5
Wojtek <pulchnymamut@gmail.com>
2024-05-11 02:40:35 +0200

    Create Image.tsx

---
+++ React/List10/src/ui/Image.tsx
@@ -0,0 +1,26 @@
+import placeholderImage from '../images/placeholder.png'
+
+const MyImage = ({ url }: { url: string }) => {
+  return (
+    <div className="w-[150px] h-[150px] mb-4">
+      {/* Placeholder Image */}
+      <img
+        src={placeholderImage}
+        alt="Placeholder"
+        className="object-cover w-full h-full"
+      />
+      {/* Main Image */}
+      <img
+        src={url}
+        alt="Question Image"
+        className="object-cover w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-300"
+        style={{ zIndex: -1 }}
+        onLoad={(e: React.SyntheticEvent<HTMLImageElement>) => {
+          (e.target as HTMLImageElement).style.opacity = '1';
+        }}
+      />
+    </div>
+  )
+}
+
+export default MyImage
