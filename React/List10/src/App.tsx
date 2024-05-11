389090e4508e27813e9673058d7272204540db74
Wojtek <pulchnymamut@gmail.com>
2024-05-11 03:06:39 +0200

    Update App.tsx

--- React/List10/src/App.tsx
+++ React/List10/src/App.tsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <InfoProvider>
       <div className="bg-gradient-to-b from-purple-900 via-purple-500 to-blue-300 h-screen flex justify-center items-center">
-        <div className="bg-blue-300 w-[600px] h-[500px] rounded-xl flex flex-col justify-between items-center">
+        <div className="bg-blue-300 w-[600px] h-[600px] rounded-xl flex flex-col justify-between items-center">
           <Topbar />
           <Main />
           <Bottombar />
