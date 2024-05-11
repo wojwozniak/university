ee546d88dbedcd5946848d1b0551ef9da93f5da2
Wojtek <pulchnymamut@gmail.com>
2024-05-11 02:57:43 +0200

    fixes to image

--- React/List10/src/ui/Image.tsx
+++ React/List10/src/ui/Image.tsx
@@ -1,8 +1,9 @@
-import placeholderImage from '../images/placeholder.png'
+import React from 'react';
+import placeholderImage from '../images/placeholder.png';
 
-const MyImage = ({ url }: { url?: string }) => {
+const MyImage = ({ url }: { url?: string | undefined }) => {
   return (
-    <div className="w-[150px] h-[150px] mb-4">
+    <div className="w-[150px] h-[150px] mb-4 relative">
       {/* Placeholder Image */}
       <img
         src={placeholderImage}
@@ -10,17 +11,19 @@ const MyImage = ({ url }: { url?: string }) => {
         className="object-cover w-full h-full"
       />
       {/* Main Image */}
-      url && <img
-        src={url}
-        alt="Question Image"
-        className="object-cover w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-300"
-        style={{ zIndex: -1 }}
-        onLoad={(e: React.SyntheticEvent<HTMLImageElement>) => {
-          (e.target as HTMLImageElement).style.opacity = '1';
-        }}
-      />
+      {url && (
+        <img
+          src={url}
+          alt="Question Image"
+          className="object-cover w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-300"
+          style={{ zIndex: -1 }}
+          onLoad={(e: React.SyntheticEvent<HTMLImageElement>) => {
+            (e.target as HTMLImageElement).style.opacity = '1';
+          }}
+        />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MyImage
+export default MyImage;
