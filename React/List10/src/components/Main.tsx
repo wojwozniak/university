f2eee965c9eab8f430c0ae1551c638c99eab0e0d
Wojtek <pulchnymamut@gmail.com>
2024-05-11 02:06:59 +0200

    loading data && loading screen

--- React/List10/src/components/Main.tsx
+++ React/List10/src/components/Main.tsx
@@ -1,20 +1,45 @@
-import { useState } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import MainContent from './MainContent';
-import { Endpoint } from '../interfaces/Endpoint';
+import { endpoints } from '../endpoints';
+import { InfoContext } from '../InfoContext';
+import { fetchData } from '../functions/fetchData';
+
+
 
 const Main = () => {
-  const [loaded, setLoaded] = useState<boolean>(true);
+  const { dispatch } = useContext(InfoContext);
+  const [loaded, setLoaded] = useState<boolean>(false);
 
-  const endpoints: Endpoint[] = [
-    { category: "Potions", url: "https://api.potterdb.com/v1/potions" },
-    { category: "Spells", url: "https://api.potterdb.com/v1/spells" }
-  ];
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const items = await fetchData(endpoints);
+        items.filter(cat => cat.categoryName != "");
+
+        // Mały sleep żeby było widać że rzeczywiście jest loading screen
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        dispatch({ type: "LOAD_DATA", payload: items });
+        setLoaded(true);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        setLoaded(true);
+      }
+    };
+
+    if (!loaded) fetchItems();
+  }, []);
 
   return (
     <div className="h-4/6 w-full flex flex-col items-center justify-between p-3">
-      {loaded ? <MainContent /> : <></>}
+      {!loaded && (
+        <div className="flex justify-center items-center h-full">
+          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-400"></div>
+        </div>
+      )}
+      {loaded && <MainContent />}
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
