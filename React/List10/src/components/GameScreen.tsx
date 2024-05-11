d9f481c1937956cf49be1c818bf8c2f8001fc7e5
Wojtek <pulchnymamut@gmail.com>
2024-05-11 03:00:30 +0200

    looking accetable

--- React/List10/src/components/GameScreen.tsx
+++ React/List10/src/components/GameScreen.tsx
@@ -1,43 +1,27 @@
-import React, { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { InfoContext } from '../InfoContext';
 import { Button } from '@mui/material';
+import MyImage from '../ui/Image';
+import { Stage } from '../interfaces/Stage';
+import { generateStage } from '../functions/generateStage';
+import { CategoryData } from '../interfaces/CategoryData';
 
-// Import placeholder image
-import placeholderImage from '../placeholder.png';
 
 const GameScreen = () => {
   const { state } = useContext(InfoContext);
+  const currentCategory: CategoryData = state.data.find(cat => cat.categoryName === state.selectedCategory) as CategoryData;
+  const [stage, newStage] = useState<Stage>();
+
+  useEffect(() => {
+    newStage(generateStage(currentCategory.items));
+  }, []);
 
   return (
     <div className="flex flex-col items-center justify-center h-full">
-      {/* Question Title */}
-      <h2 className="text-2xl font-bold mb-4">{state.question.title}</h2>
-
-      {/* Image Container */}
-      <div className="w-36 h-36 mb-4">
-        {/* Placeholder Image */}
-        <img
-          src={placeholderImage}
-          alt="Placeholder"
-          className="object-cover w-full h-full"
-        />
-
-        {/* Main Image */}
-        <img
-          src={state.question.image}
-          alt="Question Image"
-          className="object-cover w-full h-full absolute top-0 left-0 opacity-0 transition-opacity duration-300"
-          style={{ zIndex: -1 }}
-          // Show the main image once it's loaded
-          onLoad={(e) => {
-            e.target.style.opacity = 1;
-          }}
-        />
-      </div>
-
-      {/* Answer Options */}
+      <h2 className="text-2xl font-bold mb-4">{stage?.question}</h2>
+      <MyImage url={stage?.imageUrl} />
       <div className="flex flex-col gap-4">
-        {state.question.options.map((option, index) => (
+        {stage?.answers.map((option, index) => (
           <Button key={index} variant="contained" color="primary">
             {option}
           </Button>
