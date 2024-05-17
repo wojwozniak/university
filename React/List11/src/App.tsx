b216b39596b4ba6ad10119f1887c6bb837cdc2ef
Wojtek <pulchnymamut@gmail.com>
2024-05-18 00:50:15 +0200

    basic book table setup

--- React/List11/src/App.tsx
+++ React/List11/src/App.tsx
@@ -1,9 +1,11 @@
+import BookTable from "./components/BookTable"
 import Header from "./components/Header"
 
 function App() {
   return (
     <div>
       <Header />
+      <BookTable />
     </div>
 
   )
