5e239925b2f4c98e68e92c26a409d6af43b13cb5
Wojtek <pulchnymamut@gmail.com>
2024-05-18 01:09:09 +0200

    Add popup handling

--- React/List11/src/App.tsx
+++ React/List11/src/App.tsx
@@ -1,10 +1,25 @@
 import BookTable from "./components/table/BookTable"
 import Header from "./components/Header"
+import Popup from "./ui/Popup";
+import { useState } from "react";
 
 function App() {
+  const [isOpen, setOpen] = useState(false);
+
+  const openPopup = () => {
+    setOpen(true);
+  }
+
+  const closePopup = () => {
+    setOpen(false);
+  }
+
   return (
     <div>
-      <Header />
+      <Popup open={isOpen} onClose={closePopup} onSave={() => { }}>
+        <p>test</p>
+      </Popup>
+      <Header openPopup={openPopup} />
       <BookTable />
     </div>
 
--- React/List11/src/components/Header.tsx
+++ React/List11/src/components/Header.tsx
@@ -1,9 +1,21 @@
+import { Button } from '@mui/material';
 import React from 'react';
 
-const Header: React.FC = () => {
+interface HeaderProps {
+  openPopup: () => void
+}
+
+const Header: React.FC<HeaderProps> = ({ openPopup }) => {
+
   return (
-    <header className='w-full h-fit bg-blue-500 p-5 text-gray-50'>
-      <h1 className="text-2xl">Bookstore ABC</h1>
+    <header className='w-full h-fit bg-blue-500 p-5 text-gray-50 flex flex-row justify-between'>
+      <h1 className="text-2xl">
+        Bookstore ABC
+      </h1>
+      <Button variant='contained' color='success' onClick={openPopup}>
+        Add book
+      </Button>
+
     </header>
   );
 };
