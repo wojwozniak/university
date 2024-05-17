08e993b3eb0d78c5dff0d8eeb3250ce01e6ab4d2
Wojtek <pulchnymamut@gmail.com>
2024-05-17 20:57:34 +0200

    create basic header

---
+++ React/List11/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className='w-full h-fit bg-blue-500 p-5 text-gray-50'>
+      <h1 className="text-2xl">Bookstore ABC</h1>
+    </header>
+  );
+};
+
+export default Header;
