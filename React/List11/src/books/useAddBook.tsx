f2f2acade2592a1c25726cddb65a30376c90ef06
Wojtek <pulchnymamut@gmail.com>
2024-05-18 03:02:46 +0200

    implement adding

--- React/List11/src/books/useAddBook.tsx
+++ React/List11/src/books/useAddBook.tsx
@@ -1,12 +1,13 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { Book } from "../interfaces/Book";
 
-async function addBook(title: string) {
-  const res = await fetch("http://localhost:3000/posts", {
+async function addBook(book: Book) {
+  const res = await fetch("http://localhost:3000/books", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
-    // #TODO --- body: JSON.stringify({ title, author: "me" }),
+    body: JSON.stringify(book),
   });
   return await res.json();
 }
@@ -17,9 +18,7 @@ function useAddBook() {
   return useMutation({
     mutationFn: addBook,
     onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ["books", "list"],
-      });
+      queryClient.invalidateQueries({ queryKey: ["books", "list"] });
     },
   });
 }
