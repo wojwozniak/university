71bc5abd537a10918384ade62427cdebb705540d
Wojtek <pulchnymamut@gmail.com>
2024-05-20 14:22:44 +0200

    rework loader to accomodate multiple error handling

--- React/List11/src/ui/Loader.tsx
+++ React/List11/src/ui/Loader.tsx
@@ -1,19 +1,35 @@
 import React, { ReactNode } from 'react';
+
 interface LoaderProps {
   isLoading: boolean;
-  error: any;
+  error: any | any[];
   children: ReactNode;
 }
 
+const ErrorDisplay: React.FC<{ error: any | any[] }> = ({ error }) => {
+  if (Array.isArray(error)) {
+    return (
+      <div>
+        {error.map((err, index) => (
+          <p key={index}>{err}</p>
+        ))}
+      </div>
+    );
+  }
+  return <p>{error}</p>;
+};
+
 const Loader: React.FC<LoaderProps> = ({ isLoading, error, children }) => {
   if (isLoading) {
-    return <div className="w-full h-full">
-      Loading...
-    </div>;
+    return <p>Error loading books</p>;
   }
 
-  if (error) {
-    return <p>Error loading books</p>;
+  let displayError = false;
+  if (Array.isArray(error)) {
+    error.forEach(err => (err === true ? displayError = true : null))
+  } else if (error) { displayError = true }
+  if (displayError) {
+    return <ErrorDisplay error={error} />;
   }
 
   return <>{children}</>;
