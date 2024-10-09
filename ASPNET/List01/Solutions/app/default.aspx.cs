902eb2df0d0e2683ec0a2676d307fdd0a1f988be
Wojtek <pulchnymamut@gmail.com>
2024-10-09 19:04:27 +0200

    task10 page code

--- ASPNET/List01/Solutions/app/default.aspx.cs
+++ ASPNET/List01/Solutions/app/default.aspx.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,7 +12,29 @@ namespace list01task05
   {
     protected void Page_Load(object sender, EventArgs e)
     {
+      string filePath = @"D:\pages\example.txt";
 
+      try
+      {
+        using (StreamReader sr = new StreamReader(filePath))
+        {
+          string fileContent = sr.ReadToEnd();
+          Response.Write("<p>Zawartość pliku:</p>");
+          Response.Write("<pre>" + fileContent + "</pre>");
+        }
+      }
+      catch (FileNotFoundException ex)
+      {
+        Response.Write("<p>Nie znaleziono pliku: " + ex.Message + "</p>");
+      }
+      catch (UnauthorizedAccessException ex)
+      {
+        Response.Write("<p>Brak uprawnień: " + ex.Message + "</p>");
+      }
+      catch (Exception ex)
+      {
+        Response.Write("<p>Wystąpił błąd: " + ex.Message + "</p>");
+      }
     }
 
     protected void Button1_Click(object sender, EventArgs e)
