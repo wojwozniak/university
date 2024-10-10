4dea5aa08f0d00eea18df5707465e8e74a750471
Wojtek <pulchnymamut@gmail.com>
2024-10-10 22:04:01 +0200

    Update task04.sql

--- AplikacjeZBazami/List01/Solutions/task04.sql
+++ AplikacjeZBazami/List01/Solutions/task04.sql
@@ -17,4 +17,5 @@ SELECT *
 		( SELECT DISTINCT PC.ParentProductCategoryID FROM SalesLT.ProductCategory PC );
 
 -- this query shows us there are only four categories that has children 
--- (we can also check entire ProductCategory -> then we can see that only ParentProductCategoryID values lie between 1-4 (inc.))
+-- (we can also check entire ProductCategory -> then we can see that all ParentProductCategoryID values lie between 1-4 (inc.))
+-- so we have very simple tree -> 4 roots that have only leaves
