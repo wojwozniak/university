5c23bcbc94f15c3e115741fe8ac50de40f678226
Wojtek <pulchnymamut@gmail.com>
2024-10-10 21:26:12 +0200

    Create task02.sql

---
+++ AplikacjeZBazami/List01/Solutions/task02.sql
@@ -0,0 +1,8 @@
+SELECT PM.Name, COUNT(PM.ProductModelID) 'Count'
+	FROM SalesLT.ProductModel PM
+	JOIN SalesLT.Product P ON P.ProductModelID = PM.ProductModelID
+	GROUP BY PM.Name
+	HAVING COUNT('Count') > 1
+	ORDER BY 'Count' DESC;
+	-- -> grouping by id and not names to avoid potential problems
+	-- -> like risk of grouping different products with same name
