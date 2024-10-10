60755d0cb4ef676d3c30b1f535f404ec20c76f7e
Wojtek <pulchnymamut@gmail.com>
2024-10-10 23:27:17 +0200

    Create task09.sql

---
+++ AplikacjeZBazami/List01/Solutions/task09.sql
@@ -0,0 +1,17 @@
+-- add column
+ALTER TABLE SalesLT.Customer
+	ADD CreditCardNumber NVARCHAR(50);
+
+-- set anything to CreditCardApprovalCode in 3 random rows
+UPDATE TOP(3) SalesLT.SalesOrderHeader
+	SET CreditCardApprovalCode = 'APPROVED'
+	WHERE CreditCardApprovalCode IS NULL; 
+
+-- set CreditCardNumber to X in rows with CreditCardApprovalCode
+UPDATE SalesLT.Customer
+	SET CreditCardNumber = 'X' 
+	WHERE CustomerID IN (
+		SELECT DISTINCT SOH.CustomerID
+		FROM SalesLT.SalesOrderHeader SOH
+		WHERE SOH.CreditCardApprovalCode IS NOT NULL
+	);
