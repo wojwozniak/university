2bd77aff37dbba950defc7d3aa02f76cf9130aa4
Wojtek <pulchnymamut@gmail.com>
2024-10-10 22:08:26 +0200

    fix03

--- AplikacjeZBazami/List01/Solutions/task03.sql
+++ AplikacjeZBazami/List01/Solutions/task03.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT A.City, COUNT(CA.CustomerID) 'Customers', COUNT(DISTINCT C.SalesPerson) 'SalePersons'
+SELECT DISTINCT A.City, COUNT(DISTINCT CA.CustomerID) 'Customers', COUNT(DISTINCT C.SalesPerson) 'SalePersons'
 	FROM SalesLT.CustomerAddress CA
 	JOIN SalesLT.Address A ON A.AddressID = CA.AddressID
 	JOIN SalesLT.Customer C ON C.CustomerID = CA.CustomerID
