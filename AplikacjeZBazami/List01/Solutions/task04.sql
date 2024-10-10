07134fa87989dbe00978318e7f619f5c5621fe2f
Wojtek <pulchnymamut@gmail.com>
2024-10-10 22:01:37 +0200

    Create task04.sql

---
+++ AplikacjeZBazami/List01/Solutions/task04.sql
@@ -0,0 +1,20 @@
+-- not leaf -> has children
+-- node X has children if any row Y considers X as it's parent
+
+SELECT P.Name, PC.Name 'CategoryName'
+	FROM SalesLT.ProductCategory PC
+	JOIN SalesLT.Product P ON PC.ProductCategoryID = P.ProductID
+	WHERE PC.ProductCategoryID IN 
+		( SELECT DISTINCT PC.ParentProductCategoryID FROM SalesLT.ProductCategory PC );
+
+
+-- our query returns no rows because of current rows
+
+-- we can check it by running this:
+SELECT *
+	FROM SalesLT.ProductCategory PC
+	WHERE PC.ProductCategoryID IN 
+		( SELECT DISTINCT PC.ParentProductCategoryID FROM SalesLT.ProductCategory PC );
+
+-- this query shows us there are only four categories that has children 
+-- (we can also check entire ProductCategory -> then we can see that only ParentProductCategoryID values lie between 1-4 (inc.))
