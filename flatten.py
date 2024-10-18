fe243a71ee357e451ae3c158e0a52278235564f3
Wojtek <pulchnymamut@gmail.com>
2024-10-18 19:32:58 +0200

    rename (2/2)

--- flatten.py
+++ flatten.py
@@ -1,5 +1,7 @@
+# Utility function
 import os
 import shutil
+
 
 def flatten_folder(source_folder, destination_folder):
     for root, _, files in os.walk(source_folder):
@@ -12,8 +14,10 @@ def flatten_folder(source_folder, destination_folder):
                 index = 1
                 while os.path.exists(os.path.join(destination_folder, f"{base}_{index}{ext}")):
                     index += 1
-                destination_path = os.path.join(destination_folder, f"{base}_{index}{ext}")
+                destination_path = os.path.join(
+                    destination_folder, f"{base}_{index}{ext}")
             shutil.move(source_path, destination_path)
+
 
 # Example usage:
 source_folder = r"C:\Users\wozni\Desktop\source"
