51e98365a3a7afba1823d9f8e009536ebc1b61c8
Wojtek <pulchnymamut@gmail.com>
2024-10-20 20:37:05 +0200

    Create task01.sql

---
+++ Course_DatabaseApplications/List02/Solutions/task01.sql
@@ -0,0 +1,13 @@
+CREATE OR ALTER FUNCTION late_books (@days_overdue INT)
+	RETURNS TABLE
+	AS
+	RETURN
+(
+    SELECT C.PESEL, COUNT(DISTINCT W.Wypozyczenie_ID) 'Late Books'
+		FROM Wypozyczenie W
+		JOIN Czytelnik C ON W.Czytelnik_ID = C.Czytelnik_ID
+		WHERE W.Liczba_Dni >= @days_overdue
+		GROUP BY C.PESEL
+);
+
+SELECT * FROM late_books(14);
