9ff937ea5e42e186653bf984035542bfce59d487
Wojtek <pulchnymamut@gmail.com>
2024-12-04 15:43:50 +0100

    bugfix

--- Database_Applications/List06/Solutions/task4/task4.js
+++ Database_Applications/List06/Solutions/task4/task4.js
@@ -1,12 +1,13 @@
-// query 1 - we just adjust skip and limit properly
+
+// query 1 - we just adjust skip and limit to 2
+let totalCount = db.booksvalidated.countDocuments();
+let start = Math.floor((totalCount - 2) / 2);
+
 db.booksvalidated.find()
     .sort({ title: 1 })
-    .skip(1)
+    .skip(start)
     .limit(2)
     .pretty();
-
-// if we want to autocalculate we can use countDocuments() function
-let totalCount = db.booksvalidated.countDocuments();
 
 // query 2 - some condition applied on the nested structure
 db.booksvalidated.find(
