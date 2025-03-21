6758b0022279f2f9a1d20c15177471d3cd4b5d54
Wojtek <pulchnymamut@gmail.com>
2025-03-21 15:50:34 +0100

    validate arguments

---
+++ Computer_Networks/Assigment01/solution/validate.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int validate_arguments(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr, "Error: Provide an IP adress!\n");
+        return 1;
+    }
+    if (argc > 2)
+    {
+        fprintf(stderr, "Error: Too many arguments!\n");
+        return 1;
+    }
+    if (inet_addr(argv[1]) == (in_addr_t)(-1))
+    {
+        fprintf(stderr, "Error: Incorrect IP adress!\n");
+        return 1;
+    }
+    return 0;
+}
---
+++ Computer_Networks/Assigment01/solution/validate.h
@@ -0,0 +1,6 @@
+/**
+ *  Function that validates arguments provided by user, specifically:
+ *      - if user provided exactly one argument
+ *      - if the argument provided is a correct IP adress
+ */
+int validate_arguments(int argc, char *argv[]);
