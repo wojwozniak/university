fc8730230546c1b2e1d2e17e555936be8a1b66ad
Wojtek <pulchnymamut@gmail.com>
2025-03-21 22:19:44 +0100

    cleanup unnecessary function

--- Computer_Networks/Assigment01/solution/time_util.c
+++ Computer_Networks/Assigment01/solution/time_util.c
@@ -8,8 +8,3 @@ long long get_current_time_ms()
     gettimeofday(&tv, NULL);
     return tv.tv_sec * 1000LL;
 }
-
-long long get_remaining_time_ms(long long deadline)
-{
-    return deadline - get_current_time_ms();
-}
--- Computer_Networks/Assigment01/solution/time_util.h
+++ Computer_Networks/Assigment01/solution/time_util.h
@@ -2,8 +2,3 @@
  * Function returning how long to wait for packets
  */
 long long get_current_time_ms();
-
-/**
- * Function returning how long to wait for packets
- */
-long long get_remaining_time_ms(long long deadline);
