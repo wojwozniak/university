c8f26391716e131a1a328ebcf099ce9a128f0b84
Wojtek <pulchnymamut@gmail.com>
2025-03-22 01:29:19 +0100

    output

---
+++ Computer_Networks/Assigment01/solution/output.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+#include <poll.h>
+#include <arpa/inet.h>
+#include <netinet/ip_icmp.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+
+int print_output(int ttl, int res_count, char *target_str, uint32_t *ip_addresses, long long *timestamps)
+{
+    printf("%d. ", ttl);
+    if (res_count == 0)
+    {
+        printf("*\n");
+        return 1;
+    }
+
+    char sender_ip_str[res_count][20];
+    for (int i = 0; i < res_count; i++)
+    {
+        inet_ntop(AF_INET, &ip_addresses[i], sender_ip_str[i], sizeof(sender_ip_str[i]));
+    }
+
+    printf("%s ", sender_ip_str[0]);
+    for (int i = 1; i < res_count; i++)
+    {
+        if (strcmp(sender_ip_str[i], sender_ip_str[i - 1]) != 0)
+        {
+            printf("%s ", sender_ip_str[i]);
+        }
+    }
+
+    if (res_count == 3)
+    {
+        printf(" %lld ms\n", (timestamps[0] + timestamps[1] + timestamps[2]) / 3);
+    }
+    else
+    {
+        printf(" ???\n");
+    }
+
+    for (int i = 1; i < res_count; i++)
+    {
+        if (strcmp(sender_ip_str[i], target_str) == 0)
+        {
+            return 0;
+        }
+    }
+
+    return 1;
+}
---
+++ Computer_Networks/Assigment01/solution/output.h
@@ -0,0 +1,6 @@
+/**
+ * Auxiliary function to print out line of output
+ * Also checks for destination IP - returns 0 if we are there and should stop
+ * returns 1 if we should continue traceroute
+ */
+int print_output(int ttl, int res_count, char *target_str, uint32_t *ip_addresses, long long *timestamps);
