9f1d8f797b5d0532ad9407397e6d915c678e043e
Wojtek <pulchnymamut@gmail.com>
2025-04-12 16:37:37 +0200

    Update task4.ml

---
+++ OCaml/List05/task4.ml
@@ -0,0 +1,36 @@
+(* Zadanie 4 / Lista 4 *)
+(*
+  Rozbuduj funkcję parens_ok z poprzedniego zadania tak, by sprawdzała poprawne
+  nawiasowania składające się z symboli ’(’, ’)’, ’[’, ’]’, ’{’ oraz ’}’. 
+  Wskazówka: W tym wypadku nawias zamykający może zamknąć tylko nawias otwierający tego samego typu. Nie wystarczy więc pamiętać, ile było
+  nawiasów, ale trzeba także wiedzieć, jakiego były rodzaju. Dobrą strukturą do
+  tego jest stos, na który wrzucamy nawiasy otwierające, które zdejmujemy, gdy
+  zobaczymy pasujący nawias zamykający. Stos można zaimplementować przy
+  użyciu listy.
+*)
+
+let list_of_string s = String . to_seq s |> List . of_seq;;
+
+let parens_ok s =
+  let rec it stack lst =
+    match lst with
+      | [] -> if stack = [] then true else false
+      | x::ys -> 
+        match x with
+        | '(' | '{' | '[' -> it (x::stack) ys
+        | ')' -> 
+          (match stack with
+            | '(' :: rest -> it rest ys
+            | _ -> false)
+        | '}' -> 
+          (match stack with
+            | '{' :: rest -> it rest ys
+            | _ -> false)
+        | ']' -> 
+           (match stack with
+            | '[' :: rest -> it rest ys
+            | _ -> false)
+        | _ -> false
+  in it [] (list_of_string s);;
+
+  parens_ok "[()[{}]]";;
